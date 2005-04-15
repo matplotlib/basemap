2cac1c897b30a9491f301db1876043d5b72643cb
Jeff Whitaker <jswhit@fastmail.fm>
2005-04-15 11:31:28 +0000

    Updated to use new transform_scalar and transform_vector methods.
    svn path=/trunk/toolkits/basemap/; revision=1172

--- examples/test.py
+++ examples/test.py
@@ -3,57 +3,9 @@
 # country boundaries, filling continents and drawing
 # parallels/meridians
 
-from matplotlib.toolkits.basemap import Basemap, interp
+from matplotlib.toolkits.basemap import Basemap, shiftgrid
 from pylab import *
 import cPickle
-
-def shiftgrid(lon0,datain,lonsin,start=True):
-    """ 
- shift global lat/lon grid east or west.
- assumes wraparound (or cyclic point) is included.
-
- lon0:  starting longitude for shifted grid 
-        (ending longitude if start=False). lon0 must be on
-        input grid (with the range of lonsin).
- datain:  original data.
- lonsin:  original longitudes.
- start[True]: if True, lon0 represents he starting longitude 
- of the new grid. if False, lon0 is the ending longitude.
-
- returns dataout,lonsout (data and longitudes on shifted grid).
-    """
-    if fabs(lonsin[-1]-lonsin[0]-360.) > 1.e-4:
-        raise ValueError, 'cyclic point not included'
-    if lon0 < lonsin[0] or lon0 > lonsin[-1]:
-        raise ValueError, 'lon0 outside of range of lonsin'
-    i0 = argsort(fabs(lonsin-lon0))[0]
-    dataout = zeros(datain.shape,datain.typecode())
-    lonsout = zeros(lonsin.shape,lonsin.typecode())
-    if start:
-        lonsout[0:len(lonsin)-i0] = lonsin[i0:]
-    else:
-        lonsout[0:len(lonsin)-i0] = lonsin[i0:]-360.
-    dataout[:,0:len(lonsin)-i0] = datain[:,i0:]
-    if start:
-        lonsout[len(lonsin)-i0:] = lonsin[1:i0+1]+360.
-    else:
-        lonsout[len(lonsin)-i0:] = lonsin[1:i0+1]
-    dataout[:,len(lonsin)-i0:] = datain[:,1:i0+1]
-    return dataout,lonsout
-
-def addcyclic(arrin,lonsin):
-   """
- add cyclic (wraparound) point in longitude.
-   """
-   nlats = arrin.shape[0]
-   nlons = arrin.shape[1]
-   arrout  = zeros((nlats,nlons+1),arrin.typecode())
-   arrout[:,0:nlons] = arrin[:,:]
-   arrout[:,nlons] = arrin[:,0]
-   lonsout = zeros(nlons+1,lonsin.typecode())
-   lonsout[0:nlons] = lonsin[:]
-   lonsout[nlons]  = lonsin[-1] + lonsin[1]-lonsin[0]
-   return arrout,lonsout
 
 # read in topo data from pickle (on a regular lat/lon grid)
 # longitudes go from 20 to 380.
@@ -97,10 +49,9 @@ show()
 m = Basemap(-180.,-80.,180.,80.,\
             resolution='c',area_thresh=10000.,projection='merc',\
             lon_0=0.5*(lons[0]+lons[-1]),lat_ts=20.)
-# define grid (nx x ny regularly spaced native projection grid)
+# transform to nx x ny regularly spaced native projection grid
 nx = len(lons); ny = int(80.*len(lats)/90.)
-lonsout, latsout = m.makegrid(nx,ny)
-topodat = interp(topoin,lons,lats,lonsout,latsout)
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 fig.add_axes([0.1,0.1,0.75,0.75])
@@ -123,12 +74,9 @@ show()
 m = Basemap(-145.5,1.,-2.566,46.352,\
             resolution='c',area_thresh=10000.,projection='lcc',\
             lat_1=50.,lon_0=-107.)
-# define grid (nx x ny regularly spaced native projection grid)
+# transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
-lonsout, latsout = m.makegrid(nx,ny)
-# adjust longitudes to be consistent with etopo data
-# (which goes from 20 to 380, instead of 0 to 360).
-topodat = interp(topoin,lons,lats,lonsout,latsout)
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
@@ -158,13 +106,9 @@ show()
 m = Basemap(-10.,20.,55.,75.,
             resolution='l',projection='aea',\
             lat_1=40.,lat_2=60,lon_0=35.)
-# define grid (nx x ny regularly spaced native projection grid)
+# transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
-lonsout, latsout = m.makegrid(nx,ny)
-# adjust longitudes to be consistent with etopo data
-# (which goes from 20 to 380, instead of 0 to 360).
-lonsout = where(lonsout < lons[0], lonsout+360., lonsout)
-topodat = interp(topoin,lons,lats,lonsout,latsout)
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
@@ -198,12 +142,9 @@ show()
 m = Basemap(-150.,20.826,30.,20.826,
             resolution='c',area_thresh=10000.,projection='stere',\
             lat_0=-90.,lon_0=-105.,lat_ts=-90.)
-# define grid (nx x ny regularly spaced native projection grid)
+# transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
-lonsout, latsout = m.makegrid(nx,ny)
-# adjust longitudes to be consistent with etopo data
-lonsout = where(lonsout < lons[0], lonsout+360., lonsout)
-topodat = interp(topoin,lons,lats,lonsout,latsout)
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
@@ -228,12 +169,9 @@ show()
 m = Basemap(-150.,-20.826,30.,-20.826,
             resolution='c',area_thresh=10000.,projection='laea',\
             lat_0=90.,lon_0=-105.,lat_ts=90.)
-# define grid (nx x ny regularly spaced native projection grid)
+# transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
-lonsout, latsout = m.makegrid(nx,ny)
-# adjust longitudes to be consistent with etopo data
-lonsout = where(lonsout < lons[0], lonsout+360., lonsout)
-topodat = interp(topoin,lons,lats,lonsout,latsout)
+topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
