b2f5d746be5cb92cc222838de2fca8718e01b1de
Jeff Whitaker <jswhit@fastmail.fm>
2005-04-17 12:42:51 +0000

    No longer need to set_xlim, set_ylim manually.
    svn path=/trunk/toolkits/basemap/; revision=1204

--- examples/nytolondon.py
+++ examples/nytolondon.py
@@ -10,10 +10,18 @@ m = Basemap(-90.,30.,30.,60.,\
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 fig.add_axes([0.1,0.1,0.8,0.8])
-ax = gca() # get current axis instance
-ax.update_datalim(((m.llcrnrx, m.llcrnry),(m.urcrnrx,m.urcrnry)))
-ax.set_xlim((m.llcrnrx, m.urcrnrx))
-ax.set_ylim((m.llcrnry, m.urcrnry))
+# nylat, nylon are lat/lon of New York
+nylat = 40.78
+nylon = -73.98
+# lonlat, lonlon are lat/lon of London.
+lonlat = 51.53
+lonlon = 0.08
+# find 1000 points along the great circle.
+#x,y = m.gcpoints(nylon,nylat,lonlon,lonlat,1000)
+# draw the great circle.
+#m.plot(x,y,linewidth=2)
+# drawgreatcircle performs the previous 2 steps in one call.
+m.drawgreatcircle(nylon,nylat,lonlon,lonlat,linewidth=2,color='b')
 m.drawcoastlines()
 m.fillcontinents()
 # draw parallels
@@ -22,13 +30,5 @@ m.drawparallels(circles,labels=[1,1,0,1])
 # draw meridians
 meridians = [-90,-60,-30,0,30]
 m.drawmeridians(meridians,labels=[1,1,0,1])
-# nylat, nylon are lat/lon of New York
-nylat = 40.78
-nylon = -73.98
-# lonlat, lonlon are lat/lon of London.
-lonlat = 51.53
-lonlon = 0.08
-# draw the great circle.
-m.drawgreatcircle(nylon,nylat,lonlon,lonlat,linewidth=2,color='b')
 title('Great Circle from New York to London')
 show()
