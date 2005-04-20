a3f756a0db132e3e5387fe8d50e4b97ca59f50cb
Jeff Whitaker <jswhit@fastmail.fm>
2005-04-20 20:00:32 +0000

    tested with contour,contourf and pcolor.
    svn path=/trunk/toolkits/basemap/; revision=1228

--- examples/plotmap.py
+++ examples/plotmap.py
@@ -19,13 +19,20 @@ m = Basemap(-145.5,1.,-2.566,46.352,\
             lat_1=50.,lon_0=-107.)
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
-topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
+topodat,x,y = m.transform_scalar(topoin,lons,lats,nx,ny,returnxy=True)
 # set up figure with same aspect ratio as map.
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
-# plot image over map.
+# plot image over map with imshow.
 im = m.imshow(topodat,cm.jet)
+# use pcolor (looks very similar to imshow, but slower).
+#m.pcolor(x,y,topodat,shading='flat')
+# plot contour lines over map (colors is a list so neg contours will
+# not be dashed).
+#levels, colls = m.contour(x,y,topodat,15,linewidths=0.5,colors=['k','k'])
+# fill the contours.
+#levels, colls = m.contourf(x,y,topodat,15,cmap=cm.jet,colors=None)
 cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes
 colorbar(tickfmt='%d', cax=cax) # draw colorbar
 axes(ax)  # make the original axes current again
