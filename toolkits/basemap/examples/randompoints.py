ca6f3058198418167ffefff564245c2185fa54cc
nobody <nobody@localhost>
2005-04-29 12:25:52 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_3_3'.
    svn path=/tags/v0_3_3/; revision=1247

--- toolkits/basemap/examples/randompoints.py
+++ toolkits/basemap/examples/randompoints.py
@@ -62,14 +62,13 @@ for xi,yi in zip(x,y):
 # first, create figure with same aspect ratio as map.
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
-# background color will be used for 'wet' areas.
-fig.add_axes([0.1,0.1,0.8,0.8],axisbg='aqua')
+fig.add_axes([0.1,0.1,0.8,0.8])
 # use zorder=10 to make sure markers are drawn last.
 # (otherwise they are covered up when continents are filled)
 m.scatter(xx,yy,marker='o',c='k',s=25,zorder=10)
 # draw coasts and fill continents.
 m.drawcoastlines()
-m.fillcontinents(color='coral')
+m.fillcontinents()
 # draw parallels and meridians.
 delat = 20.
 circles = arange(0.,90.,delat).tolist()+\
