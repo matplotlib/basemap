735fee7528f4101e0964c627701f12ed0f0490c1
Jeff Whitaker <jswhit@fastmail.fm>
2005-05-14 19:10:20 +0000

    Uses pcolor to plot data on native lat/lon grid - includes orthographic example.
    svn path=/trunk/toolkits/basemap/; revision=1316

---
+++ examples/plotmap_pcolor.py
@@ -0,0 +1,99 @@
+# make plot of etopo bathymetry/topography data on
+# orthographic map projection, drawing coastlines,
+# and parallels/meridians.
+
+# this version does not interpolate the data to the projection grid.
+# instead, the x,y coordinates of the lat/lon grid are passed to pcolor.
+
+from matplotlib.toolkits.basemap import Basemap, shiftgrid
+from pylab import *
+import cPickle
+from matplotlib.numerix import ma
+
+# read in topo data from pickle (on a regular lat/lon grid)
+# longitudes go from 20 to 380.
+topodict = cPickle.load(open('etopo20.pickle','rb'))
+topodatin = topodict['data']; lonsin = topodict['lons']; latsin = topodict['lats']
+# shift lons and lats by 1/2 grid increment (so values represent the vertices
+# of the grid box surrounding the data value, as pcolor expects).
+delon = lonsin[1]-lonsin[0]
+delat = latsin[1]-latsin[0]
+lons = zeros(len(lonsin)+1,'f')
+lats = zeros(len(latsin)+1,'f')
+lons[0:len(lonsin)] = lonsin-0.5*delon
+lons[-1] = lonsin[-1]+0.5*delon
+lats[0:len(latsin)] = latsin-0.5*delon
+lats[-1] = latsin[-1]+0.5*delon
+lons, lats = meshgrid(lons, lats)
+
+# setup of basemap ('lcc' = lambert conformal conic).
+# use major and minor sphere radii from WGS84 ellipsoid.
+m = Basemap(-145.5,1.,-2.566,46.352,rsphere=(6378137.00,6356752.3142),\
+            resolution='l',area_thresh=1000.,projection='lcc',\
+            lat_1=50.,lon_0=-107.)
+xsize = rcParams['figure.figsize'][0]
+fig=figure(figsize=(xsize,m.aspect*xsize))
+ax = fig.add_axes([0.1,0.1,0.7,0.7],axisbg='aqua')
+# compute x,y of regular lat/lon grid.
+x,y = m(lons,lats)
+# make x,y masked arrays (masked where data is outside of projection limb)
+x = ma.masked_values(where(x > 1.e10,1.e10,x), 1.e10)
+y = ma.masked_values(where(y > 1.e10,1.e10,y), 1.e10)
+# make topodat a masked array, masking values lower than sea level.
+topodat = where(topodatin < 0.,1.e10,topodatin)
+topodat = ma.masked_values(topodat, 1.e10)
+# make a pcolor plot.
+p = m.pcolor(x,y,topodat,shading='flat')
+clim(-4500.,3500.) # adjust range of colors.
+cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes
+colorbar(tickfmt='%d', cax=cax) # draw colorbar
+axes(ax)  # make the original axes current again
+# plot blue dot on boulder, colorado and label it as such.
+xpt,ypt = m(-104.237,40.125) 
+m.plot([xpt],[ypt],'bo') 
+text(xpt+100000,ypt+100000,'Boulder')
+# draw coastlines and political boundaries.
+m.drawcoastlines()
+#m.drawcountries()
+#m.drawstates()
+# draw parallels and meridians.
+# label on left, right and bottom of map.
+parallels = arange(0.,80,20.)
+m.drawparallels(parallels,labels=[1,1,0,1])
+meridians = arange(10.,360.,30.)
+m.drawmeridians(meridians,labels=[1,1,0,1])
+title('ETOPO Topography - Lambert Conformal Conic')
+show()
+
+# setup of basemap ('ortho' = orthographic projection)
+# use major and minor sphere radii from WGS84 ellipsoid.
+m = Basemap(projection='ortho',rsphere=(6378137.00,6356752.3142),\
+            resolution='c',area_thresh=10000.,lat_0=30,lon_0=-60)
+xsize = rcParams['figure.figsize'][0]
+fig=figure(figsize=(xsize,m.aspect*xsize))
+ax = fig.add_axes([0.1,0.1,0.7,0.7],frameon=False)
+# compute x,y of regular lat/lon grid.
+x,y = m(lons,lats)
+# make x,y masked arrays (masked where data is outside of projection limb)
+x = ma.masked_values(where(x > 1.e10,1.e10,x), 1.e10)
+y = ma.masked_values(where(y > 1.e10,1.e10,y), 1.e10)
+topodat = ma.masked_values(topodatin, 1.e10)
+# make a pcolor plot.
+p = m.pcolor(x,y,topodat,shading='flat')
+clim(-4500.,3500.) # adjust range of colors.
+cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes
+colorbar(tickfmt='%d', cax=cax) # draw colorbar
+axes(ax)  # make the original axes current again
+# draw coastlines and political boundaries.
+m.drawcoastlines()
+# draw parallels and meridians (labelling is 
+# not implemented for orthographic).
+parallels = arange(0.,80,20.)
+m.drawparallels(parallels)
+meridians = arange(0.,360.,20.)
+m.drawmeridians(meridians)
+# draw boundary around map region.
+m.drawmapboundary()
+title('ETOPO Topography - Orthographic')
+savefig('ortho')
+show()
