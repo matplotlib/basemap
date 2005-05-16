1bec64fe0427df1569c215bb7ad823d7c2a6dc4d
Jeff Whitaker <jswhit@fastmail.fm>
2005-05-16 18:48:15 +0000

    Changed to Robinson projection.
    svn path=/trunk/toolkits/basemap/; revision=1327

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -4,22 +4,23 @@ from pylab import *
 # read in topo data from pickle (on a regular lat/lon grid)
 topodict = cPickle.load(open('etopo20.pickle','rb'))
 etopo = topodict['data']; lons = topodict['lons']; lats = topodict['lats']
-# create Basemap instance (global cylindrical equidistant is default)
-m = Basemap(lons[0],lats[0],lons[-1],lats[-1])
+# create Basemap instance for Robinson projection.
+m = Basemap(projection='robin',lon_0=0.5*(lons[0]+lons[-1]))
+# compute native map projection coordinates for lat/lon grid.
+lons, lats = meshgrid(lons,lats)
+x,y = m(lons,lats)
 # create figure with same aspect ratio as map.
-xsize = rcParams['figure.figsize'][0]
-fig=figure(figsize=(xsize,m.aspect*xsize))
-fig.add_axes([0.1,0.1,0.8,0.8])
-im = m.imshow(etopo)
-# draw coastlines and fill continents.
+fig=figure(figsize=(10,m.aspect*10))
+fig.add_axes([0.1,0.1,0.8,0.8],frameon=False)
+# make filled contour plot.
+levels, colls = m.contourf(x,y,etopo,30,cmap=cm.jet,colors=None)
+# draw coastlines.
 m.drawcoastlines()
-m.fillcontinents()
-# draw parallels, label on bottom.
-circles = arange(-90.,120.,30.)
-m.drawparallels(circles,labels=[1,0,0,0])
-# draw meridians, label on left.
-meridians = arange(0.,390.,60.)
-m.drawmeridians(meridians,labels=[0,0,0,1])
-title('Cylindrical Equidistant')
+# draw a line around the map region.
+m.drawmapboundary()
+# draw parallels and meridians.
+m.drawparallels(arange(-90.,120.,30.))
+m.drawmeridians(arange(0.,420.,60.))
+title('Robinson Projection')
 show()
 
