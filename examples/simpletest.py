1522950798097e6c8783b9673119b62a8a564c04
Jeff Whitaker <jswhit@fastmail.fm>
2005-05-16 21:31:20 +0000

    *** empty log message ***
    svn path=/trunk/toolkits/basemap/; revision=1335

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -7,11 +7,9 @@ etopo = topodict['data']; lons = topodict['lons']; lats = topodict['lats']
 # create Basemap instance for Robinson projection.
 m = Basemap(projection='robin',lon_0=0.5*(lons[0]+lons[-1]))
 # compute native map projection coordinates for lat/lon grid.
-lons, lats = meshgrid(lons,lats)
-x,y = m(lons,lats)
+x, y = m(*meshgrid(lons,lats))
 # create figure with same aspect ratio as map.
-fig=figure(figsize=(10,m.aspect*10))
-fig.add_axes([0.1,0.1,0.8,0.8],frameon=False)
+figure(figsize=(10,m.aspect*10)).add_axes([0.1,0.1,0.8,0.8],frameon=False)
 # make filled contour plot.
 levels, colls = m.contourf(x,y,etopo,30,cmap=cm.jet,colors=None)
 # draw coastlines.
@@ -21,6 +19,7 @@ m.drawmapboundary()
 # draw parallels and meridians.
 m.drawparallels(arange(-90.,120.,30.))
 m.drawmeridians(arange(0.,420.,60.))
+# add a title.
 title('Robinson Projection')
 show()
 
