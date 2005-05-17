ae09c9fedc7e083d10ee7fcd643125eb859537a3
Jeff Whitaker <jswhit@fastmail.fm>
2005-05-17 16:46:11 +0000

    examples of plotting vectors in geographical (lat/lon) and native map projection coordinates.
    svn path=/trunk/toolkits/basemap/; revision=1342

--- examples/quiver_demo.py
+++ examples/quiver_demo.py
@@ -17,25 +17,55 @@ p = reshape(array(pl,'f'),(nlats,nlons))
 lats = -90.+dellat*arange(nlats)
 lons = -180.+dellon*arange(nlons)
 
+# plot vectors in geographical (lat/lon) coordinates.
+
 # north polar projection.
 m = Basemap(-180.,10.,0.,10.,
             resolution='c',area_thresh=10000.,projection='stere',\
             lat_0=90.,lon_0=-135.,lat_ts=90.)
+# setup figure with same aspect ratio as map.
+xsize = rcParams['figure.figsize'][0]
+fig=figure(figsize=(xsize,m.aspect*xsize))
+ax = fig.add_axes([0.1,0.1,0.7,0.7])
+# rotate wind vectors to map projection coordinates.
+# (also compute native map projections coordinates of lat/lon grid)
+# only do Northern Hemisphere.
+urot,vrot,x,y = m.rotate_vector(u[36:,:],v[36:,:],lons,lats[36:],returnxy=True)
+# plot filled contours over map.
+levels, colls = m.contourf(x,y,p[36:,:],15,cmap=cm.jet,colors=None)
+# plot wind vectors over map.
+m.quiver(x,y,urot,vrot)
+cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes.
+colorbar(tickfmt='%d', cax=cax) # draw colorbar
+axes(ax)  # make the original axes current again
+m.drawcoastlines()
+m.drawcountries()
+# draw parallels
+delat = 20.
+circles = arange(0.,90.+delat,delat).tolist()+\
+          arange(-delat,-90.-delat,-delat).tolist()
+m.drawparallels(circles,labels=[1,1,1,1])
+# draw meridians
+delon = 45.
+meridians = arange(-180,180,delon)
+m.drawmeridians(meridians,labels=[1,1,1,1])
+title('Surface Winds Winds and Pressure',y=1.075)
+show()
 
-# transform from spherical to map projection coordinates.
+# plot vectors in map projection coordinates.
+
+# north polar projection.
+m = Basemap(-180.,10.,0.,10.,
+            resolution='c',area_thresh=10000.,projection='stere',\
+            lat_0=90.,lon_0=-135.,lat_ts=90.)
+# transform from spherical to map projection coordinates (rotation
+# and interpolation).
 nxv = 41; nyv = 41
 nxp = 101; nyp = 101
 spd = sqrt(u**2+v**2)
 print max(ravel(spd))
 udat, vdat, xv, yv = m.transform_vector(u,v,lons,lats,nxv,nyv,returnxy=True)
 pdat, xp, yp = m.transform_scalar(p,lons,lats,nxp,nyp,returnxy=True)
-
-print min(ravel(udat)),max(ravel(udat))
-print min(ravel(vdat)),max(ravel(vdat))
-print min(ravel(pdat)),max(ravel(pdat))
-spd = sqrt(udat**2+vdat**2)
-print max(ravel(spd))
-
 # setup figure with same aspect ratio as map.
 xsize = rcParams['figure.figsize'][0]
 fig=figure(figsize=(xsize,m.aspect*xsize))
@@ -58,95 +88,5 @@ m.drawparallels(circles,labels=[1,1,1,1])
 delon = 45.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,1,1,1])
-ax.set_xticks([]) # no ticks
-ax.set_yticks([])
-title('Surface Winds Winds and Pressure',y=1.075)
-show()
-
-# mercator projection
-m = Basemap(-180.,-80.,180.,80.,\
-            resolution='c',area_thresh=10000.,projection='merc',\
-            lon_0=0.,lat_ts=20.)
-
-# transform from spherical to map projection coordinates.
-nxv = 41; nyv = 41
-nxp = 101; nyp = 101
-udat, vdat, xv, yv = m.transform_vector(u,v,lons,lats,nxv,nyv,returnxy=True)
-pdat, xp, yp = m.transform_scalar(p,lons,lats,nxp,nyp,returnxy=True)
-
-print min(ravel(udat)),max(ravel(udat))
-print min(ravel(vdat)),max(ravel(vdat))
-print min(ravel(pdat)),max(ravel(pdat))
-spd = sqrt(udat**2+vdat**2)
-print max(ravel(spd))
-
-# setup figure with same aspect ratio as map.
-xsize = rcParams['figure.figsize'][0]
-fig=figure(figsize=(xsize,m.aspect*xsize))
-ax = fig.add_axes([0.1,0.1,0.7,0.7])
-# plot filled contours over map
-levels, colls = m.contourf(xp,yp,pdat,15,cmap=cm.jet,colors=None)
-# plot wind vectors over map.
-m.quiver(xv,yv,udat,vdat)
-cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes.
-colorbar(tickfmt='%d', cax=cax) # draw colorbar
-axes(ax)  # make the original axes current again
-m.drawcoastlines()
-m.drawcountries()
-# draw parallels
-delat = 30.
-circles = arange(0.,90.+delat,delat).tolist()+\
-          arange(-delat,-90.-delat,-delat).tolist()
-m.drawparallels(circles,labels=[1,0,0,1])
-# draw meridians
-delon = 60.
-meridians = arange(-180,180,delon)
-m.drawmeridians(meridians,labels=[1,0,0,1])
-ax.set_xticks([]) # no ticks
-ax.set_yticks([])
-title('Surface Winds Winds and Pressure',y=1.075)
-show()
-
-#cylindrical equidistant
-m = Basemap(-180,-90.,180.,90.,\
-            resolution='c',area_thresh=10000.,projection='cyl')
-
-# transform from spherical to map projection coordinates.
-# (for 'cyl' projection this does nothing).
-nxv = 41; nyv = 41
-nxp = 101; nyp = 101
-udat, vdat, xv, yv = m.transform_vector(u,v,lons,lats,nxv,nyv,returnxy=True)
-pdat, xp, yp = m.transform_scalar(p,lons,lats,nxp,nyp,returnxy=True)
-
-print min(ravel(udat)),max(ravel(udat))
-print min(ravel(vdat)),max(ravel(vdat))
-print min(ravel(pdat)),max(ravel(pdat))
-spd = sqrt(udat**2+vdat**2)
-print max(ravel(spd))
-
-# setup figure with same aspect ratio as map.
-xsize = rcParams['figure.figsize'][0]
-fig=figure(figsize=(xsize,m.aspect*xsize))
-ax = fig.add_axes([0.1,0.1,0.7,0.7])
-# plot filled contours over map
-levels, colls = m.contourf(xp,yp,pdat,15,cmap=cm.jet,colors=None)
-# plot wind vectors over map.
-m.quiver(xv,yv,udat,vdat)
-cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes.
-colorbar(tickfmt='%d', cax=cax) # draw colorbar
-axes(ax)  # make the original axes current again
-m.drawcoastlines()
-m.drawcountries()
-# draw parallels
-delat = 30.
-circles = arange(0.,90.+delat,delat).tolist()+\
-          arange(-delat,-90.-delat,-delat).tolist()
-m.drawparallels(circles,labels=[1,0,0,1])
-# draw meridians
-delon = 60.
-meridians = arange(-180,180,delon)
-m.drawmeridians(meridians,labels=[1,0,0,1])
-ax.set_xticks([]) # no ticks
-ax.set_yticks([])
 title('Surface Winds Winds and Pressure',y=1.075)
 show()
