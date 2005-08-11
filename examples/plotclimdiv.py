2250eebe7cb1b80aceaef8839367f70a12a42df8
Jeff Whitaker <jswhit@fastmail.fm>
2005-08-11 21:13:40 +0000

    minor tweaks
    svn path=/trunk/toolkits/basemap/; revision=1619

--- examples/plotclimdiv.py
+++ examples/plotclimdiv.py
@@ -15,22 +15,24 @@ m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
 fig=p.figure(figsize=(8,m.aspect*8))
 fig.add_axes([0.1,0.1,0.8,0.8])
 # draw climate division boundaries.
-shp_info = m.readshapefile('divisions','climdivs')
+shp_info = m.readshapefile('divnames','climdivs',drawbounds=True)
 print shp_info
 # make sure the shapefile has polygons (and not just lines).
 if shp_info[1] != 5:
     print 'warning: shapefile does not contain polygons'
 # choose a color for each climate division (randomly).
 colors={}
-divisions=[]
+divnames=[]
+print m.climdivs_info[0].keys()
 for nshape in range(shp_info[0]):
     divname = m.climdivs_info[nshape]['ST']+repr(m.climdivs_info[nshape]['DIV'])
     colors[divname] = (random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))
-    divisions.append(divname)
-# cycle through climate divisions, color each one.
+    divnames.append(divname)
+# cycle through climate divnames, color each one.
 for nshape,seg in enumerate(m.climdivs):
     xx,yy = zip(*seg)
-    p.fill(xx,yy,rgb2hex(colors[divisions[nshape]]))
+    color = rgb2hex(colors[divnames[nshape]]) 
+    p.fill(xx,yy,color,edgecolor=color)
 # draw meridians and parallels.
 m.drawparallels(nx.arange(25,65,20),labels=[1,0,0,0])
 m.drawmeridians(nx.arange(-120,-40,20),labels=[0,0,0,1])
