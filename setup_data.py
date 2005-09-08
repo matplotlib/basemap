344a5949658f68fba3e78e8a4b14b95212d93e61
Jeff Whitaker <jswhit@fastmail.fm>
2005-09-08 19:07:31 +0000

    setup script for data files.
    svn path=/trunk/toolkits/basemap/; revision=1731

---
+++ setup_data.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import os
+
+datadir = os.environ.get('BASEMAP_DATA_PATH')
+if not datadir:
+    datadir ='share/basemap'
+
+setup(
+  name              = "basemap-data",
+  author            = "Jeff Whitaker",
+  author_email      = "jeffrey.s.whitaker@noaa.gov",
+  data_files        = [(datadir,['data/countries_c.txt','data/states_c.txt','data/countries_l.txt','data/states_l.txt','data/gshhs_c.txt','data/gshhs_l.txt','data/countries_i.txt','data/states_i.txt','data/gshhs_i.txt','data/countries_h.txt','data/states_h.txt','data/gshhs_h.txt'])])
