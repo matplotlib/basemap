e15e79d9ce2bf2136785b9bcb6761c829697da78
Jeff Whitaker <jswhit@fastmail.fm>
2005-09-15 22:08:56 +0000

    pyshapelib now included
    svn path=/trunk/toolkits/basemap/; revision=1760

--- examples/plotclimdiv.py
+++ examples/plotclimdiv.py
@@ -5,10 +5,6 @@ from matplotlib.collections import LineCollection
 from matplotlib.colors import rgb2hex
 import random
 
-# requires pyshapelib from Thuban (http://thuban.intevation.org/).
-# cd to libraries/pyshapelib in Thuban source distribution, run
-# 'python setup.py install'.
-
 # Lambert Conformal map of lower 48 states.
 m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
             projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
