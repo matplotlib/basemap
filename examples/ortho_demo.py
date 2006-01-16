18f373ffc1e49b33708ae2303b61ccf76ffa686e
Jeff Whitaker <jswhit@fastmail.fm>
2006-01-16 12:46:42 +0000

    Use pylab.load to read in data.
    svn path=/trunk/toolkits/basemap/; revision=1957

--- examples/ortho_demo.py
+++ examples/ortho_demo.py
@@ -1,18 +1,16 @@
-from matplotlib import rcParams, use
-rcParams['numerix'] = 'Numeric'  # make sure Numeric is used (to read pickle)
 from matplotlib.toolkits.basemap import Basemap
-import cPickle
 from pylab import *
 # read in topo data from pickle (on a regular lat/lon grid)
-topodict = cPickle.load(open('etopo20.pickle','rb'))
-etopo = topodict['data']; lons = topodict['lons']; lats = topodict['lats']
+etopo = array(load('etopo20data.gz'),'f')
+lons = array(load('etopo20lons.gz'),'f')
+lats = array(load('etopo20lats.gz'),'f')
 # create Basemap instance for Orthographic (satellite view) projection.
 lon_0 = float(raw_input('enter reference longitude (lon_0):'))
 lat_0 = float(raw_input('enter reference latitude (lat_0):'))
 fillcont = int(raw_input('fill continents? (1 for yes, 0 for no):'))
 m = Basemap(projection='ortho',lon_0=lon_0,lat_0=lat_0)
 # compute native map projection coordinates for lat/lon grid.
-lons, lats = meshgrid(lons,lats)
+lons, lats = meshgrid(lons, lats)
 x,y = m(lons,lats)
 # create figure with same aspect ratio as map.
 fig=m.createfigure().add_axes([0.05,0.05,0.9,0.9])
