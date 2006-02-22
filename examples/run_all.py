914c568b393e0304d6474c4556da8573f78321ab
Jeff Whitaker <jswhit@fastmail.fm>
2006-02-22 23:27:21 +0000

    driver script to run all examples
    svn path=/trunk/toolkits/basemap/; revision=2078

---
+++ examples/run_all.py
@@ -0,0 +1,21 @@
+import glob, os, sys
+test_files = glob.glob('*.py')
+test_files.remove('run_all.py')
+test_files.remove('fcstmaps.py')
+test_files.remove('testgdal.py')
+print test_files
+py_path = os.environ.get('PYTHONPATH')
+if py_path is None:
+    py_path = '.'
+else:
+    py_path = os.pathsep.join(['.',py_path])
+os.environ['PYTHONPATH'] = py_path
+
+for f in test_files:
+    print "**********************************************"
+    ff = os.path.join(sys.path[0],f)
+    args = [sys.executable,ff]
+    print "Running",' '.join(args)
+    status = os.spawnve(os.P_WAIT,sys.executable,args,os.environ)
+    if status:
+        print 'TEST FAILURE (status=%s)' % (status)
