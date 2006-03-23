4cb59741ea17a5162e674cda846070aabdd1703f
Jeff Whitaker <jswhit@fastmail.fm>
2006-03-23 12:58:37 +0000

    Remove deprecated creatfigure
    svn path=/trunk/toolkits/basemap/; revision=2220

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -6,8 +6,6 @@ lons=array(load('etopo20lons.gz'),'d')
 lats=array(load('etopo20lats.gz'),'d')
 # create Basemap instance for Robinson projection.
 m = Basemap(projection='robin',lon_0=0.5*(lons[0]+lons[-1]))
-# create figure with same aspect ratio as map.
-fig = m.createfigure()
 # make filled contour plot.
 x, y = m(*meshgrid(lons, lats))
 cs = m.contourf(x,y,etopo,30,cmap=cm.jet)
