d25272c3d28a8480c71bd0ba6200b6b99856cdb1
Jeff Whitaker <jswhit@fastmail.fm>
2006-04-06 21:03:39 +0000

    Now only install 'i' and 'h' datasets.
    svn path=/trunk/toolkits/basemap/; revision=2269

--- data/setup.py
+++ data/setup.py
@@ -6,7 +6,7 @@ if not datadir:
     datadir ='share/basemap'
 
 setup(
-  version           = "0.8",
+  version           = "0.9",
   description       = "boundary data for basemap matplotlib toolkit",
   url               = "http://matplotlib.sourceforge.net/toolkits.html",
   download_url      = "http://sourceforge.net/projects/matplotlib",
@@ -14,4 +14,4 @@ setup(
   author_email      = "jeffrey.s.whitaker@noaa.gov",
   license           = "OSI Approved",
   name              = "basemap-data",
-  data_files        = [(datadir,['countries_c.txt','states_c.txt','countries_l.txt','states_l.txt','gshhs_c.txt','gshhs_l.txt','countries_i.txt','states_i.txt','gshhs_i.txt','countries_h.txt','states_h.txt','gshhs_h.txt','rivers_c.txt','rivers_l.txt','rivers_i.txt','rivers_h.txt'])])
+  data_files        = [(datadir,['countries_i.txt','states_i.txt','gshhs_i.txt','countries_h.txt','states_h.txt','gshhs_h.txt','rivers_i.txt','rivers_h.txt'])])
