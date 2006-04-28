b0907a59000c38a01541228cd3360828e6e1ed42
Jeff Whitaker <jswhit@fastmail.fm>
2006-04-28 12:03:18 +0000

    New example.
    svn path=/trunk/toolkits/basemap/; revision=2348

---
+++ examples/garp.py
@@ -0,0 +1,45 @@
+from matplotlib.toolkits.basemap import Basemap, proj4
+from pylab import title, show, arange
+
+# the shortest route from the center of the map
+# to any other point is a straight line in the azimuthal
+# equidistant projection. Such lines show the true scale
+# on the earth's surface.
+# So, for the specified point, this script draws a map that shows
+# in which direction to depart for other points on earth and how far
+# it will be to reach that destination.
+# The specified point shows up as a red dot in the center of the map.
+
+# This example shows how to use the proj4 module included in basemap
+# to choose the lon/lat values for the corners of the map.
+
+# user enters the lon/lat of the point, and it's name
+lon_0 = float(raw_input('input reference lon (degrees):'))
+lat_0 = float(raw_input('input reference lat (degrees):'))
+location = raw_input('name of location:')
+
+# use the proj4 module to find 4 corners of the map.
+rsphere = 6370997
+p = proj4.Proj(proj='aeqd',lon_0=lon_0,lat_0=lat_0,R=rsphere)
+# upper right corner is 10000 km from center point.
+xurcrnr,yurcrnr = 10000000,10000000
+# find the lon/lat for the lower left and upper right corners.
+llcrnrlon,llcrnrlat = p(-xurcrnr,-yurcrnr,inverse=True)
+urcrnrlon,urcrnrlat = p(xurcrnr,yurcrnr,inverse=True)
+# use these values to setup Basemap instance.
+m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,\
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,\
+            resolution='l',projection='aeqd',\
+            lat_0=lat_0,lon_0=lon_0,rsphere=rsphere)
+# draw coasts and fill continents.
+m.drawcoastlines(linewidth=0.5)
+m.fillcontinents()
+# 20 degree graticule.
+m.drawparallels(arange(-80,81,20))
+m.drawmeridians(arange(-180,180,20))
+# draw a red dot at the center.
+xpt, ypt = m(lon_0, lat_0)
+m.plot([xpt],[ypt],'ro') 
+# draw the title.
+title('The World According to Garp in '+location)
+show()
