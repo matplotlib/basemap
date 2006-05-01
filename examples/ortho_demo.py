a570a6850e0af0b43501beefc691dc462271c45e
Jeff Whitaker <jswhit@fastmail.fm>
2006-05-01 15:04:39 +0000

    docstring fix.
    svn path=/trunk/toolkits/basemap/; revision=2367

--- examples/ortho_demo.py
+++ examples/ortho_demo.py
@@ -7,7 +7,8 @@ m = Basemap(projection='ortho',lon_0=lon_0,lat_0=lat_0)
 # plot land-sea mask.
 rgba_land = (0,1,0,1)
 rgba_ocean = (0,0,1,1)
-m.drawlsmask(rgba_land, rgba_ocean)
+# lakes=True means plot inland lakes with ocean color.
+m.drawlsmask(rgba_land, rgba_ocean, lakes=True)
 # draw parallels and meridians.
 m.drawparallels(arange(-90.,120.,30.))
 m.drawmeridians(arange(0.,420.,60.))
