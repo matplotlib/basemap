0d7899773aa341ea55781d2b7c579820663fdb3d
Jeff Whitaker <jswhit@fastmail.fm>
2006-05-02 15:52:29 +0000

    Bumped revision number.
    svn path=/trunk/toolkits/basemap/; revision=2378

--- lib/matplotlib/toolkits/basemap/basemap.py
+++ lib/matplotlib/toolkits/basemap/basemap.py
@@ -26,7 +26,7 @@ if not _datadir:
    _datadir = os.path.join(sys.prefix,'share','basemap')
 
 __version__ = '0.9'
-__revision__ = '20060430'
+__revision__ = '20060502'
 
 # test to see if array indexing is supported
 # (it is not for Numeric, but is for numarray and numpy)
