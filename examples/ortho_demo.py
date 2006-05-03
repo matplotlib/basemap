55c0e8ef55d57c1561f01c78c62a6603d2986c0e
Jeff Whitaker <jswhit@fastmail.fm>
2006-05-03 16:38:01 +0000

    rgba tuples now ints, not floats.
    svn path=/trunk/toolkits/basemap/; revision=2381

--- examples/ortho_demo.py
+++ examples/ortho_demo.py
@@ -5,8 +5,8 @@ lon_0 = float(raw_input('enter reference longitude (lon_0):'))
 lat_0 = float(raw_input('enter reference latitude (lat_0):'))
 m = Basemap(projection='ortho',lon_0=lon_0,lat_0=lat_0)
 # plot land-sea mask.
-rgba_land = (0,1,0,1)
-rgba_ocean = (0,0,1,1)
+rgba_land = (0,255,0,255) # land green.
+rgba_ocean = (0,0,255,255) # ocean blue.
 # lakes=True means plot inland lakes with ocean color.
 m.drawlsmask(rgba_land, rgba_ocean, lakes=True)
 # draw parallels and meridians.
