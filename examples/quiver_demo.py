c5c021181e5630092ce5374be5206b642affe1b9
Jeff Whitaker <jswhit@fastmail.fm>
2006-07-25 15:57:28 +0000

    Don't use single character Numeric typecodes
    svn path=/trunk/toolkits/basemap/; revision=2609

--- examples/quiver_demo.py
+++ examples/quiver_demo.py
@@ -11,9 +11,9 @@ for line in file.readlines():
    ul.append(float(l[0]))
    vl.append(float(l[1]))
    pl.append(float(l[2]))
-u = reshape(array(ul,'f'),(nlats,nlons))
-v = reshape(array(vl,'f'),(nlats,nlons))
-p = reshape(array(pl,'f'),(nlats,nlons))
+u = reshape(array(ul,Float32),(nlats,nlons))
+v = reshape(array(vl,Float32),(nlats,nlons))
+p = reshape(array(pl,Float32),(nlats,nlons))
 lats1 = -90.+dellat*arange(nlats)
 lons1 = -180.+dellon*arange(nlons)
 lons, lats = meshgrid(lons1, lats1)
@@ -33,7 +33,7 @@ urot,vrot,x,y = m.rotate_vector(u[36:,:],v[36:,:],lons[36:,:],lats[36:,:],return
 # plot filled contours over map.
 cs = m.contourf(x,y,p[36:,:],15,cmap=cm.jet)
 # plot wind vectors over map.
-Q = m.quiver(x,y,urot,vrot)
+Q = m.quiver(x,y,urot,vrot) #or specify, e.g., width=0.003, scale=400)
 qk = quiverkey(Q, 0.95, 1.05, 25, '25 m/s', labelpos='W')
 cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes.
 colorbar(cax=cax) # draw colorbar
@@ -49,7 +49,7 @@ m.drawparallels(circles,labels=[1,1,1,1])
 delon = 45.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,1,1,1])
-title('Surface Winds Winds and Pressure',y=1.075)
+title('Surface Winds Winds and Pressure (lat-lon grid)',y=1.075)
 show()
  
 # plot vectors in map projection coordinates.
@@ -70,7 +70,7 @@ ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map
 im = m.imshow(pdat,cm.jet)
 # plot wind vectors over map.
-Q = m.quiver(xv,yv,udat,vdat)
+Q = m.quiver(xv,yv,udat,vdat) #or specify, e.g., width=0.003, scale=400)
 qk = quiverkey(Q, 0.95, 1.05, 25, '25 m/s', labelpos='W')
 cax = axes([0.875, 0.1, 0.05, 0.7]) # setup colorbar axes.
 colorbar(cax=cax) # draw colorbar
@@ -86,5 +86,5 @@ m.drawparallels(circles,labels=[1,1,1,1])
 delon = 45.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,1,1,1])
-title('Surface Winds Winds and Pressure',y=1.075)
+title('Surface Winds Winds and Pressure (projection grid)',y=1.075)
 show()
