ad83e929cca814881a118041b6a5befe9c873c52
Jeff Whitaker <jswhit@fastmail.fm>
2007-02-14 12:48:48 +0000

    fixed lon/lat of California city
    svn path=/trunk/toolkits/basemap/; revision=3020

--- examples/wiki_example.py
+++ examples/wiki_example.py
@@ -5,8 +5,8 @@ import pylab as p
 # use low resolution coastlines.
 map = Basemap(projection='ortho',lat_0=50,lon_0=-100,resolution='l')
 # draw coastlines, country boundaries, fill continents.
-map.drawcoastlines()
-map.drawcountries()
+map.drawcoastlines(linewidth=0.25)
+map.drawcountries(linewidth=0.25)
 map.fillcontinents(color='coral')
 # draw the edge of the map projection region (the projection limb)
 map.drawmapboundary()
@@ -14,9 +14,9 @@ map.drawmapboundary()
 map.drawmeridians(p.arange(0,360,30))
 map.drawparallels(p.arange(-90,90,30))
 # lat/lon coordinates of five cities.
-lats=[40.02,34.00,38.55,48.25,17.29]
-lons=[-105.16,-119.40,-77.00,-114.21,-88.10]
-cities=['Boulder, CO','Santa Cruz, CA',
+lats=[40.02,32.73,38.55,48.25,17.29]
+lons=[-105.16,-117.16,-77.00,-114.21,-88.10]
+cities=['Boulder, CO','San Diego, CA',
         'Washington, DC','Whitefish, MT','Belize City, Belize']
 # compute the native map projection coordinates for cities.
 x,y = map(lons,lats)
@@ -24,7 +24,7 @@ x,y = map(lons,lats)
 map.plot(x,y,'bo')
 # plot the names of those five cities.
 for name,xpt,ypt in zip(cities,x,y):
-    p.text(xpt+50000,ypt+50000,name)
+    p.text(xpt+50000,ypt+50000,name,fontsize=9)
 # make up some data on a regular lat/lon grid.
 nlats = 73; nlons = 145; delta = 2.*p.pi/(nlons-1)
 lats = (0.5*p.pi-delta*p.indices((nlats,nlons))[0,:,:])
@@ -36,4 +36,5 @@ x, y = map(lons*180./p.pi, lats*180./p.pi)
 # contour data over the map.
 cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
 p.show()
+#p.savefig('wiki_example.ps')
 
