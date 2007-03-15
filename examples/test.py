da41019632a140efc19a1646dd21161fbb0347c2
Jeff Whitaker <jswhit@fastmail.fm>
2007-03-15 11:25:18 +0000

    remove multiple show() calls
    svn path=/trunk/toolkits/basemap/; revision=3072

--- examples/test.py
+++ examples/test.py
@@ -20,11 +20,11 @@ lats = latsin
 print 'min/max etopo20 data:'
 print min(ravel(topoin)),max(ravel(topoin))
 
+# create new figure
+fig=figure()
 # setup cylindrical equidistant map projection (global domain).
 m = Basemap(llcrnrlon=-180.,llcrnrlat=-90,urcrnrlon=180.,urcrnrlat=90.,\
             resolution='c',area_thresh=10000.,projection='cyl')
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topoin,cm.jet)
@@ -46,17 +46,16 @@ delon = 60.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,0,0,1])
 title('Cylindrical Equidistant')
-print 'plotting Cylindrical Equidistant example, close plot window to proceed ...'
-show()
+print 'plotting Cylindrical Equidistant example ...'
 
+# create new figure
+fig=figure()
 # setup miller cylindrical map projection.
 m = Basemap(llcrnrlon=-180.,llcrnrlat=-90,urcrnrlon=180.,urcrnrlat=90.,\
             resolution='c',area_thresh=10000.,projection='mill')
 # transform to nx x ny regularly spaced native projection grid
 nx = len(lons); ny = len(lats)
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-#fig=figure()
 fig.add_axes([0.1,0.1,0.75,0.75])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -66,9 +65,10 @@ m.drawparallels(circles,labels=[1,1,1,1])
 # draw meridians
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Miller Cylindrical',y=1.1)
-print 'plotting Miller Cylindrical example, close plot window to proceed ...'
-show()
+print 'plotting Miller Cylindrical example ...'
 
+# create new figure
+fig=figure()
 # setup mercator map projection (-80 to +80).
 m = Basemap(llcrnrlon=-180.,llcrnrlat=-80,urcrnrlon=180.,urcrnrlat=80.,\
             resolution='c',area_thresh=10000.,projection='merc',\
@@ -76,8 +76,6 @@ m = Basemap(llcrnrlon=-180.,llcrnrlat=-80,urcrnrlon=180.,urcrnrlat=80.,\
 # transform to nx x ny regularly spaced native projection grid
 nx = len(lons); ny = int(80.*len(lats)/90.)
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 fig.add_axes([0.1,0.1,0.75,0.75])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -90,14 +88,14 @@ m.drawparallels(circles,labels=[1,1,1,1])
 # draw meridians
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Mercator',y=1.1)
-print 'plotting Mercator example, close plot window to proceed ...'
-show()
+print 'plotting Mercator example ...'
 
+# create new figure
+fig=figure()
 # setup cassini-soldner basemap.
 m = Basemap(llcrnrlon=-6,llcrnrlat=49,urcrnrlon=4,urcrnrlat=59,\
             resolution='l',area_thresh=1000.,projection='cass',\
             lat_0=54.,lon_0=-2.)
-fig=figure()
 fig.add_axes([0.125,0.2,0.6,0.6])
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/20000.)+1; ny = int((m.ymax-m.ymin)/20000.)+1
@@ -120,14 +118,14 @@ delon = 2.
 meridians = arange(-10,10,delon)
 m.drawmeridians(meridians,labels=[1,0,0,1],fontsize=10)
 title('Cassini-Soldner Projection')
-print 'plotting Cassini-Soldner example, close plot window to proceed ...'
-show()
+print 'plotting Cassini-Soldner example ...'
 
+# create new figure
+fig=figure()
 # setup gnomonic basemap.
 m = Basemap(llcrnrlon=-95.,llcrnrlat=-52,urcrnrlon=-35.,urcrnrlat=15.,\
             resolution='c',area_thresh=10000.,projection='gnom',\
             lat_0=-10.,lon_0=-60.)
-fig=figure()
 fig.add_axes([0.125,0.2,0.6,0.6])
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
@@ -151,14 +149,14 @@ delon = 20.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,0,0,1],fontsize=10)
 title('Gnomonic Projection')
-print 'plotting Gnomonic example, close plot window to proceed ...'
-show()
+print 'plotting Gnomonic example ...'
 
+# create new figure
+fig=figure()
 # setup transverse mercator basemap.
 m = Basemap(llcrnrlon=170.,llcrnrlat=-45,urcrnrlon=10.,urcrnrlat=45.,\
             resolution='c',area_thresh=10000.,projection='tmerc',\
             lat_0=0.,lon_0=-90.)
-fig=figure()
 fig.add_axes([0.125,0.2,0.6,0.6])
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
@@ -181,14 +179,14 @@ delon = 20.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,0,0,0],fontsize=10)
 title('Transverse Mercator Projection')
-print 'plotting Transverse Mercator example, close plot window to proceed ...'
-show()
+print 'plotting Transverse Mercator example ...'
 
+# create new figure
+fig=figure()
 # setup oblique mercator basemap.
 m = Basemap(llcrnrlon=-130.,llcrnrlat=39,urcrnrlon=-124.,urcrnrlat=60.,\
             resolution='l',area_thresh=1000.,projection='omerc',\
             lon_2=-140,lat_2=55,lon_1=-120,lat_1=40)
-fig=figure()
 fig.add_axes([0.125,0.2,0.6,0.6])
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/20000.)+1; ny = int((m.ymax-m.ymin)/20000.)+1
@@ -213,14 +211,14 @@ delon = 3.
 meridians = arange(-140,-120,delon)
 m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
 title('Oblique Mercator Projection')
-print 'plotting Oblique Mercator example, close plot window to proceed ...'
-show()
+print 'plotting Oblique Mercator example ...'
 
+# create new figure
+fig=figure()
 # setup polyconic basemap.
 m = Basemap(llcrnrlon=-35.,llcrnrlat=-30,urcrnrlon=80.,urcrnrlat=50.,\
             resolution='c',area_thresh=1000.,projection='poly',\
             lat_0=0.,lon_0=20.)
-fig=figure()
 fig.add_axes([0.125,0.2,0.6,0.6])
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
@@ -244,9 +242,10 @@ delon = 20.
 meridians = arange(-180,180,delon)
 m.drawmeridians(meridians,labels=[1,0,0,1],fontsize=10)
 title('Polyconic Projection')
-print 'plotting Polyconic example, close plot window to proceed ...'
-show()
+print 'plotting Polyconic example ...'
 
+# create new figure
+fig=figure()
 # setup equidistant conic
 m = Basemap(llcrnrlon=-90.,llcrnrlat=18,urcrnrlon=-70.,urcrnrlat=26.,\
             resolution='l',area_thresh=1000.,projection='eqdc',\
@@ -254,8 +253,6 @@ m = Basemap(llcrnrlon=-90.,llcrnrlat=18,urcrnrlon=-70.,urcrnrlat=26.,\
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -276,9 +273,10 @@ delon = 5.
 meridians = arange(-100,-60,delon)
 m.drawmeridians(meridians,labels=[0,0,0,1])
 title('Equidistant Conic')
-print 'plotting Equidistant Conic example, close plot window to proceed ...'
-show()
+print 'plotting Equidistant Conic example ...'
 
+# create new figure
+fig=figure()
 # setup lambert conformal map projection (North America).
 m = Basemap(llcrnrlon=-145.5,llcrnrlat=1,urcrnrlon=-2.566,urcrnrlat=46.352,\
             resolution='c',area_thresh=10000.,projection='lcc',\
@@ -286,8 +284,6 @@ m = Basemap(llcrnrlon=-145.5,llcrnrlat=1,urcrnrlon=-2.566,urcrnrlat=46.352,\
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -309,9 +305,10 @@ delon = 30.
 meridians = arange(10.,360.,delon)
 m.drawmeridians(meridians,labels=[1,1,0,1])
 title('Lambert Conformal Conic')
-print 'plotting Lambert Conformal example, close plot window to proceed ...'
-show()
+print 'plotting Lambert Conformal example ...'
 
+# create new figure
+fig=figure()
 # setup albers equal area map projection (Europe).
 m = Basemap(llcrnrlon=-10.,llcrnrlat=20,urcrnrlon=55.,urcrnrlat=75,\
             resolution='l',projection='aea',\
@@ -319,8 +316,6 @@ m = Basemap(llcrnrlon=-10.,llcrnrlat=20,urcrnrlon=55.,urcrnrlat=75,\
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -341,9 +336,10 @@ delon = 30.
 meridians = arange(10.,360.,delon)
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Albers Equal Area Conic',y=1.075)
-print 'plotting Albers Equal Area example, close plot window to proceed ...'
-show()
+print 'plotting Albers Equal Area example ...'
 
+# create new figure
+fig=figure()
 # setup stereographic map projection (Southern Hemisphere).
 #m = Basemap(llcrnrlon=120.,llcrnrlat=0.,urcrnrlon=-60.,urcrnrlat=0.,\
 #            resolution='c',area_thresh=10000.,projection='stere',\
@@ -354,8 +350,6 @@ m = Basemap(lon_0=75.,boundinglat=-20,
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -371,9 +365,10 @@ m.drawparallels(circles)
 # draw meridians
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Polar Stereographic',y=1.075)
-print 'plotting Stereographic example, close plot window to proceed ...'
-show()
+print 'plotting Stereographic example ...'
 
+# create new figure
+fig=figure()
 # setup lambert azimuthal map projection (Northern Hemisphere).
 #m = Basemap(llcrnrlon=-150.,llcrnrlat=-18.,urcrnrlon=30.,urcrnrlat=--18.,\
 #            resolution='c',area_thresh=10000.,projection='laea',\
@@ -384,8 +379,6 @@ m = Basemap(lon_0=-105,boundinglat=20.,
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -402,9 +395,10 @@ m.drawparallels(circles)
 # draw meridians
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Lambert Azimuthal Equal Area',y=1.075)
-print 'plotting Lambert Azimuthal example, close plot window to proceed ...'
-show()
+print 'plotting Lambert Azimuthal example ...'
 
+# create new figure
+fig=figure()
 # setup azimuthal equidistant map projection (Northern Hemisphere).
 #m = Basemap(llcrnrlon=-150.,llcrnrlat=40.,urcrnrlon=30.,urcrnrlat=40.,\
 #            resolution='c',area_thresh=10000.,projection='aeqd',\
@@ -415,8 +409,6 @@ m = Basemap(lon_0=-105,boundinglat=55.,
 # transform to nx x ny regularly spaced native projection grid
 nx = int((m.xmax-m.xmin)/40000.)+1; ny = int((m.ymax-m.ymin)/40000.)+1
 topodat = m.transform_scalar(topoin,lons,lats,nx,ny)
-# setup figure with same aspect ratio as map.
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map.
 im = m.imshow(topodat,cm.jet)
@@ -433,12 +425,13 @@ m.drawparallels(circles)
 # draw meridians
 m.drawmeridians(meridians,labels=[1,1,1,1])
 title('Azimuthal Equidistant',y=1.075)
-print 'plotting Azimuthal Equidistant example, close plot window to proceed ...'
-show()
+print 'plotting Azimuthal Equidistant example ...'
 
 # projections with elliptical boundaries (orthographic, sinusoidal,
 # mollweide and robinson)
 
+# create new figure
+fig=figure()
 # setup of basemap ('ortho' = orthographic projection)
 m = Basemap(projection='ortho',
             resolution='c',area_thresh=10000.,lat_0=30,lon_0=-60)
@@ -449,7 +442,6 @@ nx = int((m.xmax-m.xmin)/dx)+1; ny = int((m.ymax-m.ymin)/dx)+1
 # interpolate to native projection grid.
 # values outside of projection limb will be masked.
 topo = m.transform_scalar(topoin,lons,lats,nx,ny,masked=True)
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # set missing value in color pallette.
 palette = cm.jet
@@ -474,13 +466,13 @@ m.drawmeridians(meridians)
 # draw boundary around map region.
 m.drawmapboundary()
 title('Orthographic')
-print 'plotting Orthographic example, close plot window to proceed ...'
-show()
+print 'plotting Orthographic example ...'
 
+# create new figure
+fig=figure()
 # setup of sinusoidal ('sinu' = sinusioidal projection)
 m = Basemap(projection='sinu',
             resolution='c',area_thresh=10000.,lon_0=0.5*(lonsin[0]+lonsin[-1]))
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map with pcolormesh.
 x,y = m(*meshgrid(lonsin,latsin))
@@ -499,13 +491,13 @@ m.drawmeridians(meridians)
 # draw boundary around map region.
 m.drawmapboundary()
 title('Sinusoidal')
-print 'plotting Sinusoidal example, close plot window to proceed ...'
-show()
+print 'plotting Sinusoidal example ...'
 
+# create new figure
+fig=figure()
 # setup of basemap ('moll' = mollweide projection)
 m = Basemap(projection='moll',
             resolution='c',area_thresh=10000.,lon_0=0.5*(lonsin[0]+lonsin[-1]))
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map with pcolormesh.
 x,y = m(*meshgrid(lonsin,latsin))
@@ -524,13 +516,13 @@ m.drawmeridians(meridians)
 # draw boundary around map region.
 m.drawmapboundary()
 title('Mollweide')
-print 'plotting Mollweide example, close plot window to proceed ...'
-show()
+print 'plotting Mollweide example ...'
 
+# create new figure
+fig=figure()
 # setup of basemap ('robin' = robinson projection)
 m = Basemap(projection='robin',
             resolution='c',area_thresh=10000.,lon_0=0.5*(lonsin[0]+lonsin[-1]))
-fig=figure()
 ax = fig.add_axes([0.1,0.1,0.7,0.7])
 # plot image over map with pcolormesh.
 x,y = m(*meshgrid(lonsin,latsin))
@@ -549,7 +541,8 @@ m.drawmeridians(meridians,labels=[0,0,0,1])
 # draw boundary around map region.
 m.drawmapboundary()
 title('Robinson')
-print 'plotting Robinson example, close plot window to proceed ...'
+print 'plotting Robinson example ...'
 show()
+
 
 print 'done'
