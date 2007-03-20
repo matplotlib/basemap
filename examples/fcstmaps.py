a102f7def6c8f25404663f7fdf8cbc9da4101155
Jeff Whitaker <jswhit@fastmail.fm>
2007-03-20 20:02:00 +0000

    get rid of 'from pylab import *'
    svn path=/trunk/toolkits/basemap/; revision=3111

--- examples/fcstmaps.py
+++ examples/fcstmaps.py
@@ -7,7 +7,8 @@ try:
     from dap import client
 except:
     raise ImportError,"requires pyDAP module (version 2.1 or higher) from http://pydap.org"
-from pylab import title, show, figure, cm, arange, frange, axes, colorbar
+from pylab import title, show, figure, cm, arange, frange, figtext, \
+                  meshgrid, axes, colorbar, where, amin, amax, around
 import sys
 from matplotlib.numerix import ma
 import datetime
