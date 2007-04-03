b468f72a788be9aa6a1bdd8b2e57443db6955532
Jeff Whitaker <jswhit@fastmail.fm>
2007-04-03 14:21:32 +0000

    plot number strings, or colored markers.
    svn path=/trunk/toolkits/basemap/; revision=3152

--- examples/randompoints.py
+++ examples/randompoints.py
@@ -1,5 +1,5 @@
-from pylab import show, title, arange, figure, title, arccos, pi, cm
-import random
+from pylab import show, title, arange, figure, title, arccos, pi, cm, text
+from matplotlib.colors import rgb2hex
 from matplotlib.toolkits.basemap import Basemap
 from matplotlib.numerix.random_array import uniform
 
@@ -17,11 +17,11 @@ npts = 750
 try: # this works for numpy
     u = uniform(0.,1.,size=npts)
     v = uniform(0.,1.,size=npts)
-    z = uniform(0.,1.,size=npts)
+    z = uniform(0,100,size=npts)
 except: # this works for Numeric/numarray
     u = uniform(0.,1.,shape=npts)
     v = uniform(0.,1.,shape=npts)
-    z = uniform(0.,1.,shape=npts)
+    z = uniform(0,100,shape=npts)
 lons = 360.*u
 lats = (180./pi)*arccos(2*v-1) - 90.
 # transform lons and lats to map coordinates.
@@ -31,9 +31,19 @@ x,y = m(lons,lats)
 fig=figure()
 # background color will be used for 'wet' areas.
 fig.add_axes([0.1,0.1,0.8,0.8],axisbg='aqua')
+# draw colored markers.
 # use zorder=10 to make sure markers are drawn last.
 # (otherwise they are covered up when continents are filled)
-m.scatter(x,y,25,z,cmap=cm.jet,marker='o',faceted=False,zorder=10) 
+#m.scatter(x,y,25,z,cmap=cm.jet,marker='o',faceted=False,zorder=10) 
+# create a list of strings containing z values
+# or, plot actual numbers as color-coded text strings.
+zn = [ '%2i' % zz for zz in z ]
+# plot numbers on map, colored by value.
+for numstr,zval,xpt,ypt in zip(zn,z,x,y):
+    # only plot values inside map region.
+    if xpt > m.xmin and xpt < m.xmax and ypt > m.ymin and ypt < m.ymax:
+        hexcolor = rgb2hex(cm.jet(zval/100.)[:3])
+        text(xpt,ypt,numstr,fontsize=9,weight='bold',color=hexcolor)
 # draw coasts and fill continents.
 m.drawcoastlines(linewidth=0.5)
 m.fillcontinents(color='coral')
