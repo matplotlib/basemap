204460151c80305875d1b0921978e74f9cfafd80
Eric Firing <efiring@hawaii.edu>
2007-10-31 06:00:56 +0000

    Numpification and some reformatting of pyproj and proj
    svn path=/trunk/toolkits/basemap/; revision=4071

--- lib/matplotlib/toolkits/basemap/proj.py
+++ lib/matplotlib/toolkits/basemap/proj.py
@@ -1,4 +1,4 @@
-import matplotlib.numerix as NX
+import numpy as npy
 import pyproj
 import math
 
@@ -6,36 +6,44 @@ __version__ = '1.2.2'
 _dg2rad = math.radians(1.)
 _rad2dg = math.degrees(1.)
 
+_upper_right_out_of_bounds = (
+    'the upper right corner of the plot is not in the map projection region')
+
+_lower_left_out_of_bounds = (
+    'the lower left corner of the plot is not in the map projection region')
+
+
 class Proj(object):
     """
- peforms cartographic transformations (converts from longitude,latitude
- to native map projection x,y coordinates and vice versa) using proj 
- (http://proj.maptools.org/)
- Uses a pyrex generated C-interface to libproj.
- 
- __init__ method sets up projection information.
- __call__ method compute transformations.
- See docstrings for __init__ and __call__ for details.
+    peforms cartographic transformations (converts from longitude,latitude
+    to native map projection x,y coordinates and vice versa) using proj
+    (http://proj.maptools.org/)
+    Uses a pyrex generated C-interface to libproj.
 
- Contact: Jeff Whitaker <jeffrey.s.whitaker@noaa.gov>
+    __init__ method sets up projection information.
+    __call__ method compute transformations.
+    See docstrings for __init__ and __call__ for details.
+
+    Contact: Jeff Whitaker <jeffrey.s.whitaker@noaa.gov>
     """
 
-    def __init__(self,projparams,llcrnrlon,llcrnrlat,urcrnrlon,urcrnrlat,urcrnrislatlon=True):
+    def __init__(self,projparams,llcrnrlon,llcrnrlat,
+                      urcrnrlon,urcrnrlat,urcrnrislatlon=True):
         """
- initialize a Proj class instance.
+        initialize a Proj class instance.
 
- Input 'projparams' is a dictionary containing proj map
- projection control parameter key/value pairs.
- See the proj documentation (http://www.remotesensing.org/proj/)
- for details.
+        Input 'projparams' is a dictionary containing proj map
+        projection control parameter key/value pairs.
+        See the proj documentation (http://www.remotesensing.org/proj/)
+        for details.
 
- llcrnrlon,llcrnrlat are lon and lat (in degrees) of lower left hand corner
- of projection region.
+        llcrnrlon,llcrnrlat are lon and lat (in degrees) of lower
+        left hand corner of projection region.
 
- urcrnrlon,urcrnrlat are lon and lat (in degrees) of upper right hand corner
- of projection region if urcrnrislatlon=True (default). Otherwise, 
- urcrnrlon,urcrnrlat are x,y in projection coordinates (units meters), 
- assuming the lower left corner is x=0,y=0.
+        urcrnrlon,urcrnrlat are lon and lat (in degrees) of upper
+        right hand corner of projection region if urcrnrislatlon=True
+        (default). Otherwise, urcrnrlon,urcrnrlat are x,y in projection
+        coordinates (units meters), assuming the lower left corner is x=0,y=0.
         """
         self.projparams = projparams
         self.projection = projparams['proj']
@@ -63,8 +71,9 @@ class Proj(object):
             llcrnrx = llcrnrlon
             llcrnry = llcrnrlat
         elif self.projection == 'ortho':
-            if  llcrnrlon == -180 and llcrnrlat == -90 and urcrnrlon == 180 and urcrnrlat == 90:
-                self._fulldisk = True 
+            if (llcrnrlon == -180 and llcrnrlat == -90 and
+                urcrnrlon == 180 and urcrnrlat == 90):
+                self._fulldisk = True
                 self._proj4 = pyproj.Proj(projparams)
                 llcrnrx = -self.rmajor
                 llcrnry = -self.rmajor
@@ -75,27 +84,28 @@ class Proj(object):
                 self._proj4 = pyproj.Proj(projparams)
                 llcrnrx, llcrnry = self(llcrnrlon,llcrnrlat)
                 if llcrnrx > 1.e20 or llcrnry > 1.e20:
-                    raise ValueError('the lower left corner of the plot is not in the map projection region')
+                    raise ValueError(_lower_left_out_of_bounds)
         elif self.projection == 'geos':
             self._proj4 = pyproj.Proj(projparams)
             # find major and minor axes of ellipse defining map proj region.
             delta = 0.01
-            lats = NX.arange(0,90,delta)
+            lats = npy.arange(0,90,delta)
             lon_0 = projparams['lon_0']
-            lons = lon_0*NX.ones(len(lats),'d')
+            lons = lon_0*npy.ones(len(lats),'d')
             x, y = self._proj4(lons, lats)
             yi = (y > 1.e20).tolist()
             ny = yi.index(1)-1
             height = y[ny]
-            lons = NX.arange(lon_0,lon_0+90,delta)
-            lats = NX.zeros(len(lons),'d')
+            lons = npy.arange(lon_0,lon_0+90,delta)
+            lats = npy.zeros(len(lons),'d')
             x, y = self(lons, lats)
             xi = (x > 1.e20).tolist()
             nx = xi.index(1)-1
             width = x[nx]
             self._height = height
             self._width = width
-            if  llcrnrlon == -180 and llcrnrlat == -90 and urcrnrlon == 180 and urcrnrlat == 90:
+            if (llcrnrlon == -180 and llcrnrlat == -90 and
+                urcrnrlon == 180 and urcrnrlat == 90):
                 self._fulldisk = True
                 llcrnrx = -width
                 llcrnry = -height
@@ -105,7 +115,7 @@ class Proj(object):
                 self._fulldisk = False
                 llcrnrx, llcrnry = self(llcrnrlon,llcrnrlat)
                 if llcrnrx > 1.e20 or llcrnry > 1.e20:
-                    raise ValueError('the lower left corner of the plot is not in the map projection region')
+                    raise ValueError(_lower_left_out_of_bounds)
         elif self.projection in ['moll','robin','sinu']:
             self._proj4 = pyproj.Proj(projparams)
             xtmp,urcrnry = self(projparams['lon_0'],90.)
@@ -116,7 +126,7 @@ class Proj(object):
         # note that for 'cyl' x,y == lon,lat
         self.projparams['x_0']=-llcrnrx
         self.projparams['y_0']=-llcrnry
-        # reset with x_0, y_0. 
+        # reset with x_0, y_0.
         if self.projection != 'cyl':
             self._proj4 = pyproj.Proj(projparams)
             llcrnry = 0.
@@ -136,7 +146,7 @@ class Proj(object):
                 else:
                     urcrnrx,urcrnry = self(urcrnrlon,urcrnrlat)
                     if urcrnrx > 1.e20 or urcrnry > 1.e20:
-                        raise ValueError('the upper right corner of the plot is not in the map projection region')
+                        raise ValueError(_upper_right_out_of_bounds)
             elif self.projection == 'geos':
                 if self._fulldisk:
                     urcrnrx = 2.*self._width
@@ -144,7 +154,7 @@ class Proj(object):
                 else:
                     urcrnrx,urcrnry = self(urcrnrlon,urcrnrlat)
                     if urcrnrx > 1.e20 or urcrnry > 1.e20:
-                        raise ValueError('the upper right corner of the plot is not in the map projection region')
+                        raise ValueError(_upper_right_out_of_bounds)
             elif self.projection in ['moll','robin','sinu']:
                 xtmp,urcrnry = self(projparams['lon_0'],90.)
                 urcrnrx,xtmp = self(projparams['lon_0']+180.,0)
@@ -172,39 +182,37 @@ class Proj(object):
             self.ymax = llcrnry
             self.ymin = urcrnry
 
-    def __call__(self,x,y,inverse=False):
+    def __call__(self, *args, **kw):
+        # x,y,inverse=False):
         """
- Calling a Proj class instance with the arguments lon, lat will
- convert lon/lat (in degrees) to x/y native map projection 
- coordinates (in meters).  If optional keyword 'inverse' is
- True (default is False), the inverse transformation from x/y
- to lon/lat is performed.
+        Calling a Proj class instance with the arguments lon, lat will
+        convert lon/lat (in degrees) to x/y native map projection
+        coordinates (in meters).  If optional keyword 'inverse' is
+        True (default is False), the inverse transformation from x/y
+        to lon/lat is performed.
 
- For cylindrical equidistant projection ('cyl'), this
- does nothing (i.e. x,y == lon,lat).
+        For cylindrical equidistant projection ('cyl'), this
+        does nothing (i.e. x,y == lon,lat).
 
- lon,lat can be either scalar floats or N arrays.
+        lon,lat can be either scalar floats or N arrays.
         """
-        if self.projection == 'cyl': # for cyl x,y == lon,lat
-            return x,y
-        if inverse:
-            outx,outy = self._proj4(x,y,inverse=True)
-            if self.projection in ['merc','mill']: 
-                if self.projection == 'merc':
-                    coslat = math.cos(math.radians(self.projparams['lat_ts']))
-                    sinlat = math.sin(math.radians(self.projparams['lat_ts']))
-                else:
-                    coslat = 1.
-                    sinlat = 0.
-                # radius of curvature of the ellipse perpendicular to
-                # the plane of the meridian.
-                rcurv = self.rmajor*coslat/math.sqrt(1.-self.esq*sinlat**2)
-                try: # x a scalar or an array
-                    outx = _rad2dg*(x/rcurv) + self.llcrnrlon
-                except: # x a sequence
-                    outx = [_rad2dg*(xi/rcurv) + self.llcrnrlon for xi in x]
+        if len(args) == 1:
+            xy = args[0]
+            onearray = True
         else:
-            outx,outy = self._proj4(x,y)
+            x,y = args
+            onearray = False
+        if self.projection == 'cyl': # for cyl x,y == lon,lat
+            if onearray:
+                return xy
+            else:
+                return x,y
+        inverse = kw.get('inverse', False)
+        if onearray:
+            outxy = self._proj4(xy, inverse=inverse)
+        else:
+            outx,outy = self._proj4(x, y, inverse=inverse)
+        if inverse:
             if self.projection in ['merc','mill']:
                 if self.projection == 'merc':
                     coslat = math.cos(math.radians(self.projparams['lat_ts']))
@@ -215,27 +223,69 @@ class Proj(object):
                 # radius of curvature of the ellipse perpendicular to
                 # the plane of the meridian.
                 rcurv = self.rmajor*coslat/math.sqrt(1.-self.esq*sinlat**2)
-                try: # x is a scalar or an array
-                    outx = rcurv*_dg2rad*(x-self.llcrnrlon)
-                except: # x is a sequence.
-                    outx = [rcurv*_dg2rad*(xi-self.llcrnrlon) for xi in x]
-        return outx,outy
+                if onearray:
+                    outxy[:,0] = _rad2dg*(xy[:,0]/rcurv) + self.llcrnrlon
+                else:
+                    try: # x a scalar or an array
+                        outx = _rad2dg*(x/rcurv) + self.llcrnrlon
+                    except: # x a sequence
+                        outx = [_rad2dg*(xi/rcurv) + self.llcrnrlon for xi in x]
+        else:
+            if self.projection in ['merc','mill']:
+                if self.projection == 'merc':
+                    coslat = math.cos(math.radians(self.projparams['lat_ts']))
+                    sinlat = math.sin(math.radians(self.projparams['lat_ts']))
+                else:
+                    coslat = 1.
+                    sinlat = 0.
+                # radius of curvature of the ellipse perpendicular to
+                # the plane of the meridian.
+                rcurv = self.rmajor*coslat/math.sqrt(1.-self.esq*sinlat**2)
+                if onearray:
+                    outxy[:,0] = rcurv*_dg2rad*(xy[:,0]-self.llcrnrlon)
+                else:
+                    try: # x is a scalar or an array
+                        outx = rcurv*_dg2rad*(x-self.llcrnrlon)
+                    except: # x is a sequence.
+                        outx = [rcurv*_dg2rad*(xi-self.llcrnrlon) for xi in x]
+        if onearray:
+            return outxy
+        else:
+            return outx, outy
 
     def makegrid(self,nx,ny,returnxy=False):
         """
- return arrays of shape (ny,nx) containing lon,lat coordinates of
- an equally spaced native projection grid.
- if returnxy=True, the x,y values of the grid are returned also.
+        return arrays of shape (ny,nx) containing lon,lat coordinates of
+        an equally spaced native projection grid.
+        if returnxy=True, the x,y values of the grid are returned also.
         """
         dx = (self.urcrnrx-self.llcrnrx)/(nx-1)
         dy = (self.urcrnry-self.llcrnry)/(ny-1)
-        x = self.llcrnrx+dx*NX.indices((ny,nx),NX.Float32)[1,:,:]
-        y = self.llcrnry+dy*NX.indices((ny,nx),NX.Float32)[0,:,:]
+        x = self.llcrnrx+dx*npy.indices((ny,nx),npy.float32)[1,:,:]
+        y = self.llcrnry+dy*npy.indices((ny,nx),npy.float32)[0,:,:]
         lons, lats = self(x, y, inverse=True)
         if returnxy:
             return lons, lats, x, y
         else:
             return lons, lats
+
+    def makegrid3d(self,nx,ny,returnxy=False):
+        """
+        return array of shape (ny,nx, 2) containing lon,lat coordinates of
+        an equally spaced native projection grid.
+        if returnxy=True, the x,y values of the grid are returned also.
+        """
+        dx = (self.urcrnrx-self.llcrnrx)/(nx-1)
+        dy = (self.urcrnry-self.llcrnry)/(ny-1)
+        xy = npy.empty((ny,nx,2), npy.float64)
+        xy[...,0] = self.llcrnrx+dx*npy.indices((ny,nx),npy.float32)[1,:,:]
+        xy[...,1] = self.llcrnry+dy*npy.indices((ny,nx),npy.float32)[0,:,:]
+        lonlat = self(xy, inverse=True)
+        if returnxy:
+            return lonlat, xy
+        else:
+            return lonlat
+
 
 if __name__ == "__main__":
 
@@ -247,10 +297,10 @@ if __name__ == "__main__":
     params['lon_0'] = -107
     nx = 349; ny = 277; dx = 32463.41; dy = dx
     awips221 = Proj(params,-145.5,1.0,(nx-1)*dx,(ny-1)*dy,urcrnrislatlon=False)
-# AWIPS grid 221 parameters
-# (from http://www.nco.ncep.noaa.gov/pmb/docs/on388/tableb.html)
+    # AWIPS grid 221 parameters
+    # (from http://www.nco.ncep.noaa.gov/pmb/docs/on388/tableb.html)
     llcornerx, llcornery = awips221(-145.5,1.)
-# find 4 lon/lat corners of AWIPS grid 221.
+    # find 4 lon/lat corners of AWIPS grid 221.
     llcornerx = 0.; llcornery = 0.
     lrcornerx = dx*(nx-1); lrcornery = 0.
     ulcornerx = 0.; ulcornery = dy*(ny-1)
@@ -270,13 +320,22 @@ if __name__ == "__main__":
     print '   -68.318 0.897'
     print '   -2.566 46.352'
     print '   148.639 46.635'
-# compute lons and lats for the whole AWIPS grid 221 (377x249).
+    # compute lons and lats for the whole AWIPS grid 221 (377x249).
     import time; t1 = time.clock()
     lons, lats = awips221.makegrid(nx,ny)
     t2 = time.clock()
     print 'compute lats/lons for all points on AWIPS 221 grid (%sx%s)' %(nx,ny)
     print 'max/min lons'
-    print min(NX.ravel(lons)),max(NX.ravel(lons))
+    print min(npy.ravel(lons)),max(npy.ravel(lons))
     print 'max/min lats'
-    print min(NX.ravel(lats)),max(NX.ravel(lats))
+    print min(npy.ravel(lats)),max(npy.ravel(lats))
     print 'took',t2-t1,'secs'
+    print 'Same thing but with a single 3-D array'
+    t1 = time.clock()
+    lonlat, xy = awips221.makegrid3d(nx,ny, returnxy=True)
+    t2 = time.clock()
+    print 'took',t2-t1,'secs'
+
+    assert (lons==lonlat[...,0]).all(), "The longitudes are different"
+    assert (lats==lonlat[...,1]).all(), "The latitudes are different"
+
