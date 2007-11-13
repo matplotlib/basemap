79370186e598f34f33bf8be476832a485cc6470b
Jeff Whitaker <jswhit@fastmail.fm>
2007-11-13 20:52:54 +0000

    obsolete
    svn path=/trunk/toolkits/basemap-testing/; revision=4252

--- lib/shapely/geometry/__init__.py
+++
@@ -1,9 +0,0 @@
-from geo import asShape
-from point import Point, asPoint
-from linestring import LineString, asLineString
-from polygon import Polygon, asPolygon
-from multipoint import MultiPoint, asMultiPoint
-from multilinestring import MultiLineString, asMultiLineString
-from multipolygon import MultiPolygon, asMultiPolygon
-from collection import GeometryCollection
-
