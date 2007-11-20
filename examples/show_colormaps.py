4f92e09da2ed8377a35bf1e6ff60e556e337c4cb
Jeff Whitaker <jswhit@fastmail.fm>
2007-11-20 04:08:52 +0000

    change outerproduct to outer
    svn path=/trunk/toolkits/basemap-testing/; revision=4385

--- examples/show_colormaps.py
+++ examples/show_colormaps.py
@@ -1,16 +1,16 @@
-from pylab import *
+import numpy, pylab
 from matplotlib.toolkits.basemap import cm
-a=outerproduct(arange(0,1,0.01),ones(10))
-figure(figsize=(10,7))
-subplots_adjust(top=0.8,bottom=0.05,left=0.01,right=0.99)
+a=numpy.outer(numpy.arange(0,1,0.01),numpy.ones(10))
+pylab.figure(figsize=(10,7))
+pylab.subplots_adjust(top=0.8,bottom=0.05,left=0.01,right=0.99)
 maps=[m for m in cm.datad.keys() if not m.endswith("_r")]
 maps.sort()
 l=len(maps)+1
 i=1
 for m in maps:
-    subplot(1,l,i)
-    axis("off")
-    imshow(a,aspect='auto',cmap=cm.__dict__[m],origin="lower")
-    title(m,rotation=90,fontsize=10)
+    pylab.subplot(1,l,i)
+    pylab.axis("off")
+    pylab.imshow(a,aspect='auto',cmap=cm.__dict__[m],origin="lower")
+    pylab.title(m,rotation=90,fontsize=10)
     i=i+1
-show()
+pylab.show()
