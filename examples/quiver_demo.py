9e674f0e40480a7591d49bafba65f82a8b5dd756
Jeff Whitaker <jswhit@fastmail.fm>
2007-11-22 16:17:03 +0000

    fix quiver_demo
    svn path=/trunk/toolkits/basemap/; revision=4422

--- examples/quiver_demo.py
+++ examples/quiver_demo.py
@@ -1,6 +1,7 @@
 from matplotlib.toolkits.basemap import Basemap
-from pylab import show, title, arange, meshgrid, cm, figure, sqrt, \
-                  colorbar, axes, gca, reshape, array, Float32, quiverkey
+import numpy
+from pylab import show, title, arange, meshgrid, cm, figure, \
+                  colorbar, axes, gca, reshape, quiverkey
 
 # read in data.
 file = open('fcover.dat','r')
@@ -12,9 +13,9 @@ for line in file.readlines():
    ul.append(float(l[0]))
    vl.append(float(l[1]))
    pl.append(float(l[2]))
-u = reshape(array(ul,Float32),(nlats,nlons))
-v = reshape(array(vl,Float32),(nlats,nlons))
-p = reshape(array(pl,Float32),(nlats,nlons))
+u = reshape(numpy.array(ul,numpy.float32),(nlats,nlons))
+v = reshape(numpy.array(vl,numpy.float32),(nlats,nlons))
+p = reshape(numpy.array(pl,numpy.float32),(nlats,nlons))
 lats1 = -90.+dellat*arange(nlats)
 lons1 = -180.+dellon*arange(nlons)
 lons, lats = meshgrid(lons1, lats1)
@@ -61,7 +62,7 @@ m = Basemap(lon_0=-135,boundinglat=25,
 # and interpolation).
 nxv = 41; nyv = 41
 nxp = 101; nyp = 101
-spd = sqrt(u**2+v**2)
+spd = numpy.sqrt(u**2+v**2)
 udat, vdat, xv, yv = m.transform_vector(u,v,lons1,lats1,nxv,nyv,returnxy=True)
 pdat, xp, yp = m.transform_scalar(p,lons1,lats1,nxp,nyp,returnxy=True)
 # create a figure, add an axes.
