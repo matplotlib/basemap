55b8cef58369d25a2280fcb09f98649de740f003
Jeff Whitaker <jswhit@fastmail.fm>
2007-12-08 02:35:27 +0000

    really color by pop rank
    svn path=/trunk/toolkits/basemap/; revision=4671

--- examples/plotcities.py
+++ examples/plotcities.py
@@ -1,18 +1,24 @@
 import pylab as p
+from matplotlib.mlab import prctile_rank
 import numpy
 from matplotlib.toolkits.basemap import Basemap as Basemap
 
 # cities colored by population rank.
+
 m = Basemap()
 shp_info = m.readshapefile('cities','cities')
 x, y = zip(*m.cities)
 pop = []
 for item in m.cities_info:
-    pop.append(int(item['POPULATION']))
-pop = numpy.array(pop)
-poprank = numpy.argsort(pop)
+    population = item['POPULATION']
+    if population < 0: continue # population missing
+    pop.append(population)
+popranks = prctile_rank(pop,100)
+colors = []
+for rank in popranks:
+    colors.append(p.cm.jet(float(rank)/100.))
 m.drawcoastlines()
 m.fillcontinents()
-m.scatter(x,y,25,poprank,cmap=p.cm.jet_r,marker='o',faceted=False,zorder=10)
+m.scatter(x,y,25,colors,marker='o',faceted=False,zorder=10)
 p.title('City Locations colored by Population Rank')
 p.show()
