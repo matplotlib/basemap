d12394ed79a26e5e1af7c02e3349f633b44bfb94
Jeff Whitaker <jswhit@fastmail.fm>
2008-01-11 23:59:04 +0000

    remove separate data package (won't work unless nested namespace packages are used anyway)
    svn path=/trunk/toolkits/basemap/; revision=4861

--- setupegg-data.py
+++
@@ -1,8 +0,0 @@
-"""
-Poor man's setuptools script...
-"""
-
-from setuptools import setup
-execfile('setup-data.py',
-         {'additional_params' :
-         {'namespace_packages' : ['mpl_toolkits']}})
