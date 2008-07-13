d47c5350508ae4b2906500f2bfab81ce307d65f0
Jeff Whitaker <jswhit@fastmail.fm>
2008-07-13 13:27:33 +0000

    update
    svn path=/trunk/toolkits/basemap/; revision=5762

--- doc/users/figures/aea.py
+++ doc/users/figures/aea.py
@@ -6,7 +6,7 @@ from matplotlib.patches import Polygon
 # lat_1 is first standard parallel.
 # lat_2 is second standard parallel.
 # lon_0,lat_0 is central point.
-m = Basemap(width=8000000,height=6000000,
+m = Basemap(width=8000000,height=7000000,
             resolution='l',projection='aea',\
             lat_1=40.,lat_2=60,lon_0=35,lat_0=50)
 m.drawcoastlines()
@@ -18,7 +18,7 @@ m.drawmeridians(np.arange(-180.,181.,20.))
 m.drawmapboundary(fill_color='aqua') 
 # draw tissot's indicatrix to show distortion.
 ax = plt.gca()
-for y in np.linspace(m.ymax/20,19*m.ymax/20,9):
+for y in np.linspace(m.ymax/20,19*m.ymax/20,10):
     for x in np.linspace(m.xmax/20,19*m.xmax/20,12):
         lon, lat = m(x,y,inverse=True)
         seg = m.tissot(lon,lat,1.,100)
