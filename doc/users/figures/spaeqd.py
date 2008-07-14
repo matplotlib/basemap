672fbfe948fe5219c4167d52a166f10dd23b1611
Jeff Whitaker <jswhit@fastmail.fm>
2008-07-14 11:55:41 +0000

    final examples added.
    svn path=/trunk/toolkits/basemap/; revision=5766

--- doc/users/figures/spaeqd.py
+++ doc/users/figures/spaeqd.py
@@ -1,12 +1,11 @@
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
-# setup stereographic basemap.
-# lat_ts is latitude of true scale.
-# lon_0,lat_0 is central point.
-m = Basemap(width=12000000,height=8000000,
-            resolution='l',projection='stere',\
-            lat_ts=50,lat_0=50,lon_0=-107.)
+# setup south polar aimuthal equidistant basemap.
+# The longitude lon_0 is at 6-o'clock, and the
+# latitude circle boundinglat is tangent to the edge  
+# of the map at lon_0.
+m = Basemap(projection='spaeqd',boundinglat=-10,lon_0=90,resolution='l')
 m.drawcoastlines()
 m.fillcontinents(color='coral',lake_color='aqua')
 # draw parallels and meridians.
@@ -15,10 +14,10 @@ m.drawmeridians(np.arange(-180.,181.,20.))
 m.drawmapboundary(fill_color='aqua') 
 # draw tissot's indicatrix to show distortion.
 ax = plt.gca()
-for y in np.linspace(m.ymax/20,19*m.ymax/20,9):
-    for x in np.linspace(m.xmax/20,19*m.xmax/20,12):
+for y in np.linspace(19*m.ymin/20,m.ymin/20,10):
+    for x in np.linspace(19*m.xmin/20,m.xmin/20,10):
         lon, lat = m(x,y,inverse=True)
-        poly = m.tissot(lon,lat,1.5,100,\
+        poly = m.tissot(lon,lat,2.5,100,\
                         facecolor='green',zorder=10,alpha=0.5)
-plt.title("Stereographic Projection")
-plt.savefig('stere.png')
+plt.title("South Polar Azimuthal Equidistant Projection")
+plt.savefig('spaeqd.png')
