6c2c493587247715a29f44ae9abf64a8afc7b91d
Jeff Whitaker <jswhit@fastmail.fm>
2008-11-07 12:30:08 +0000

    added masked array support to addcyclic function (thanks to Jesper Larsen)
    svn path=/trunk/toolkits/basemap/; revision=6369

--- lib/mpl_toolkits/basemap/__init__.py
+++ lib/mpl_toolkits/basemap/__init__.py
@@ -3679,8 +3679,14 @@ def shiftgrid(lon0,datain,lonsin,start=True):
     if lon0 < lonsin[0] or lon0 > lonsin[-1]:
         raise ValueError, 'lon0 outside of range of lonsin'
     i0 = np.argmin(np.fabs(lonsin-lon0))
-    dataout = np.zeros(datain.shape,datain.dtype)
-    lonsout = np.zeros(lonsin.shape,lonsin.dtype)
+    if hasattr(datain,'mask'):
+        datout  = ma.zeros((nlats,nlons+1),datain.dtype)
+    else:
+        datout  = np.zeros((nlats,nlons+1),datain.dtype)
+    if hasattr(lonsin,'mask'):
+        lonsout = ma.zeros(nlons+1,lonsin.dtype)
+    else:
+        lonsout = np.zeros(nlons+1,lonsin.dtype)
     if start:
         lonsout[0:len(lonsin)-i0] = lonsin[i0:]
     else:
