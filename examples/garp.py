d7505e87f4457fa6fcd252c3224bbfb11e36f259
Jeff Whitaker <jswhit@fastmail.fm>
2008-12-31 14:41:54 +0000

    fix bug in fulldisk aeqd, change garp.py example to plot whole disk
    svn path=/trunk/toolkits/basemap/; revision=6720

--- examples/garp.py
+++ examples/garp.py
@@ -11,20 +11,12 @@ import matplotlib.pyplot as plt
 # it will be to reach that destination.
 # The specified point shows up as a red dot in the center of the map.
 
-# This example shows how to use the width and height keywords
-# to specify the map projection region (instead of specifying
-# the lat/lon of the upper right and lower left corners).
-
 # user enters the lon/lat of the point, and it's name
 lon_0 = float(raw_input('input reference lon (degrees):'))
 lat_0 = float(raw_input('input reference lat (degrees):'))
 location = raw_input('name of location:')
 
-# use these values to setup Basemap instance.
-width = 28000000
-m = Basemap(width=width,height=width,\
-            resolution='c',projection='aeqd',\
-            lat_0=lat_0,lon_0=lon_0)
+m = Basemap(resolution='c',projection='aeqd',lat_0=lat_0,lon_0=lon_0)
 # fill background.
 m.drawmapboundary(fill_color='aqua')
 # draw coasts and fill continents.
