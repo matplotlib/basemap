8c303dd06273e375596819eda718193fe5b5461e
Jeff Whitaker <jswhit@fastmail.fm>
2009-11-07 13:29:12 +0000

    obsolete - replaced by geos-3.1.1
    svn path=/trunk/toolkits/basemap/; revision=7949

--- geos-2.2.3/swig/ruby/test/geos_tests.rb
+++
@@ -1,8 +0,0 @@
-# Runs the test cases that have been developed for GEOS
-
-require 'test_envelope'
-require 'test_geom'
-require 'test_io'
-require 'test_operations'
-require 'test_relations'
-require 'test_combinations'
