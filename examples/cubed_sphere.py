fed1073774dc77032af1d88aef86548643259449
Jeff Whitaker <jswhit@fastmail.fm>
2009-11-26 19:37:06 +0000

    fix_aspect=False so no whitespace appears on resize.
    svn path=/trunk/toolkits/basemap/; revision=7986

--- examples/cubed_sphere.py
+++ examples/cubed_sphere.py
@@ -1,11 +1,14 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
+
 # 'cubed sphere'
 # inscribe the sphere in a cube, then separately project each cube
 # face with gnomonic projection.
 # http://www.progonos.com/furuti/MapProj/Normal/ProjPoly/Foldout/Cube/cube.html
 # suitable for cutting and folding.
+
+# choose figure size to match aspect ratio of map.
 fig = plt.figure(figsize=(10,7.5))
 fig.subplots_adjust(bottom=0, left=0, right=1, top=1, wspace=0, hspace=0)
 rsphere = 6370997.
@@ -18,16 +21,18 @@ for lat_0 in [90,0,-90]:
             ax=fig.add_subplot(3,4,npanel)
             ax.set_frame_on(False)
             lon_0=225 + 90*(ncol+1) - 45
+            # use fix_aspect=False, so white space won't appear between
+            # faces of cube when window is resized.
             m = Basemap(width=width,height=height,resolution=None,\
                         projection='gnom',lon_0=lon_0,lat_0=lat_0,\
-                        rsphere=rsphere)
+                        rsphere=rsphere,fix_aspect=False)
             m.bluemarble(scale=0.5)
             m.drawparallels(np.arange(-90,91,10),color='0.5')
             m.drawmeridians(np.arange(5,365,10),color='0.5')
             #m.drawlsmask(ocean_color='aqua',land_color='coral')
             #m.drawparallels(np.arange(-90,91,10))
             #m.drawmeridians(np.arange(5,365,10))
-fig.text    (0.625,0.75,\
-            'World Map on a Cube\n Gnomonic Projection',\
-            fontsize=14)
+fig.text(0.625,0.75,\
+        'World Map on a Cube\n Gnomonic Projection',\
+        fontsize=14)
 plt.show()
