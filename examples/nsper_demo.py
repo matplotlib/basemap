1af8076632974997f95eed478a23ab821c7ba4c7
Jeff Whitaker <jswhit@fastmail.fm>
2010-02-11 13:15:28 +0000

    add limited area plot
    svn path=/trunk/toolkits/basemap/; revision=8126

--- examples/nsper_demo.py
+++ examples/nsper_demo.py
@@ -21,5 +21,23 @@ m.drawparallels(np.arange(-90.,120.,10.))
 m.drawmeridians(np.arange(0.,420.,20.))
 m.drawmapboundary()
 plt.title('Near-Sided Perspective Map Centered on Lon=%s, Lat=%s, H=%g' %\
-    (lon_0,lat_0,h),fontsize=10)
+    (lon_0,lat_0,h/1000.),fontsize=10)
+
+fig = plt.figure()
+m1 = Basemap(projection='nsper',lon_0=lon_0,lat_0=lat_0,satellite_height=h,resolution=None)
+ax = fig.add_axes([0.1,0.1,0.8,0.8],axisbg='k')
+# plot just upper right quadrant (coordinates determined from global map).
+m = Basemap(projection='nsper',lon_0=lon_0,lat_0=lat_0,satellite_height=h,resolution='l',llcrnrx=0.,llcrnry=0.,urcrnrx=m1.urcrnrx/2.,urcrnry=m1.urcrnry/2.)
+m.drawcoastlines()
+m.drawmapboundary(fill_color='aqua')
+m.fillcontinents(color='coral',lake_color='aqua')
+m.drawcountries()
+m.drawstates()
+# draw parallels and meridians.
+m.drawparallels(np.arange(-90.,120.,30.))
+m.drawmeridians(np.arange(0.,420.,60.))
+m.drawmapboundary()
+plt.title('Near-Sided Perspective Map Centered on Lon=%s, Lat=%s, H=%g' %\
+    (lon_0,lat_0,h/1000.),fontsize=10)
+
 plt.show()
