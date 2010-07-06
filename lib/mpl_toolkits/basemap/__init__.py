2cfb7b4054994c056ff787c9697d828f0098a03e
Jeff Whitaker <jswhit@fastmail.fm>
2010-07-06 01:43:34 +0000

    change version to 1.0 (following matplotlib's lead)
    svn path=/trunk/toolkits/basemap/; revision=8500

--- lib/mpl_toolkits/basemap/__init__.py
+++ lib/mpl_toolkits/basemap/__init__.py
@@ -52,7 +52,7 @@ if 'BASEMAPDATA' in os.environ:
 else:
     basemap_datadir = os.sep.join([os.path.dirname(__file__), 'data'])
 
-__version__ = '0.99.5'
+__version__ = '1.0'
 
 # supported map projections.
 _projnames = {'cyl'      : 'Cylindrical Equidistant',
