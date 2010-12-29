fb5b482e21e76a77084383968d6952b81de6510f
Jeff Whitaker <jswhit@fastmail.fm>
2010-12-29 21:43:26 +0000

    add some more projections, label parallels and meridians.
    svn path=/trunk/toolkits/basemap/; revision=8862

--- examples/plot_tissot.py
+++ examples/plot_tissot.py
@@ -25,9 +25,11 @@ m4 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='npstere')
 m5 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='nplaea')
 m6 = Basemap(lon_0=0,projection='moll')
 m7 = Basemap(lon_0=0,projection='robin')
-m8 = Basemap(lon_0=0,projection='mbtfpq')
+m8 = Basemap(lon_0=0,projection='hammer')
+m9 = Basemap(lon_0=0,projection='mbtfpq')
+m10 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='npaeqd')
 
-for m in [m1,m2,m3,m4,m5,m6,m7,m8]:
+for m in [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10]:
     # make a new figure.
     fig = plt.figure()
     # draw "circles" at specified longitudes and latitudes.
@@ -35,8 +37,8 @@ for m in [m1,m2,m3,m4,m5,m6,m7,m8]:
         for meridian in range(-165,166,30):
             poly = m.tissot(meridian,parallel,6,100,facecolor='green',zorder=10,alpha=0.5)
     # draw meridians and parallels.
-    m.drawparallels(np.arange(-60,61,30))
-    m.drawmeridians(np.arange(-180,180,60))
+    m.drawparallels(np.arange(-60,61,30),labels=[1,0,0,0])
+    m.drawmeridians(np.arange(-180,180,60),labels=[0,0,0,1])
     # draw coastlines, fill continents, plot title.
     m.drawcoastlines()
     m.drawmapboundary(fill_color='aqua') 
