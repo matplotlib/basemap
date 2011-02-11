c7ad169edc8e8ed9afc092a95bc5278939666dd2
Jeff Whitaker <jswhit@fastmail.fm>
2011-02-11 20:52:28 +0000

    use rita data from AOML for lic example.
    svn path=/trunk/toolkits/basemap/; revision=8972

--- examples/lic_demo.py
+++ examples/lic_demo.py
@@ -13,14 +13,14 @@ try:
 except ImportError:
     raise  ImportError('need vectorplot scikit for this example')
 
-date = '2010090100'
-lat0=22.6; lon0=-69.2
-
-ncfile = NetCDFFile('hurrearl.nc')
-u = ncfile.variables['u10m'][:,:]
-v = ncfile.variables['v10m'][:,:]
-lons1 = ncfile.variables['lon'][:]
-lats1 = ncfile.variables['lat'][:]
+# H*wind data from http://www.aoml.noaa.gov/hrd/data_sub/wind.html
+ncfile = NetCDFFile('rita.nc')
+u = ncfile.variables['sfc_u'][0,:,:]
+v = ncfile.variables['sfc_v'][0,:,:]
+lons1 = ncfile.variables['longitude'][:]
+lats1 = ncfile.variables['latitude'][:]
+lat0 = lats1[len(lats1)/2]; lon0 = lons1[len(lons1)/2]
+print lat0,lon0
 lons, lats = np.meshgrid(lons1,lats1)
 ncfile.close()
 
@@ -36,6 +36,6 @@ im = m.imshow(image,plt.cm.gist_stern)
 m.drawcoastlines()
 m.drawmeridians(np.arange(0,360,2),labels=[0,0,0,1])
 m.drawparallels(np.arange(-30,30,2),labels=[1,0,0,0])
-plt.title('Hurricane Earl flow field visualized with Line Integral Convolution',\
+plt.title('Hurricane Rita flow field visualized with Line Integral Convolution',\
         fontsize=13)
 plt.show()
