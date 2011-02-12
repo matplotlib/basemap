1cd414c715d762802408195cc94004e3970a386e
Jeff Whitaker <jswhit@fastmail.fm>
2011-02-12 16:10:15 +0000

    add comments.
    svn path=/trunk/toolkits/basemap/; revision=8982

--- examples/lic_demo.py
+++ examples/lic_demo.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 try:
     from scikits.vectorplot import lic_internal
 except ImportError:
-    raise  ImportError('need vectorplot scikit for this example')
+    raise ImportError('need vectorplot scikit for this example')
 
 # H*wind data from http://www.aoml.noaa.gov/hrd/data_sub/wind.html
 ncfile = NetCDFFile('rita.nc')
@@ -20,7 +20,7 @@ lat0 = lats1[len(lats1)/2]; lon0 = lons1[len(lons1)/2]
 lons, lats = np.meshgrid(lons1,lats1)
 ncfile.close()
 
-# downsample to finer grid.
+# downsample to finer grid for nicer looking plot.
 nlats = 2*udat.shape[0]; nlons = 2*udat.shape[1]
 lons = np.linspace(lons1[0],lons1[-1],nlons)
 lats = np.linspace(lats1[0],lats1[-1],nlats)
@@ -31,11 +31,13 @@ vdat = interp(vdat,lons1,lats1,lons,lats,order=3)
 
 fig = plt.figure(figsize=(8,8))
 m = Basemap(projection='cyl',llcrnrlat=lats1[0],llcrnrlon=lons1[0],urcrnrlat=lats1[-1],urcrnrlon=lons1[-1],resolution='i')
+# pass texture, kernel and data to LIC function from vectorplot.
 kernellen=31
 texture = np.random.rand(udat.shape[0],udat.shape[1]).astype(np.float32)
 kernel = np.sin(np.arange(kernellen)*np.pi/kernellen).astype(np.float32)
 image = lic_internal.line_integral_convolution(udat.astype(np.float32),\
         vdat.astype(np.float32), texture, kernel)
+# plot the resulting image.
 im = m.imshow(image,plt.cm.gist_stern)
 m.drawcoastlines()
 m.drawmeridians(np.arange(-120,-60,2),labels=[0,0,0,1])
