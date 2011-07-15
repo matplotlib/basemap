428e62e5d03154ac1b4566f803016a5c1ae0f8eb
Jeff Whitaker <jswhit@fastmail.fm>
2011-07-15 13:58:04 -0600

    modifications to makelsmask example

--- examples/makelsmask.py
+++ examples/makelsmask.py
@@ -3,13 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 # make land-sea-lake mask from built-in coastline dataset using is_land method.
 # this is very slow!
-lons = np.arange(-180,181,2.5)
-lats = np.arange(-90,91,2.5)
+minutes = 150
+resolution = 'c'
+
+delon = minutes/60.
+nlons = int(360./delon); nlons = nlons + 1
+nlats = int(180./delon); nlats = nlats + 1
+print minutes,nlons,nlats,resolution
+lons = np.linspace(-180,180,nlons)
+lats = np.linspace(-90,90,nlats)
 lsmask = np.zeros((len(lats),len(lons)),dtype=np.uint8)
-m = Basemap(llcrnrlon=-180,llcrnrlat=-90,urcrnrlon=180,urcrnrlat=90,resolution='c',projection='cyl')
+print lsmask.shape, lsmask.dtype
+
+m =\
+Basemap(llcrnrlon=-180,llcrnrlat=-90,urcrnrlon=180,urcrnrlat=90,resolution=resolution,projection='cyl')
 for j,lat in enumerate(lats):
     for i,lon in enumerate(lons):
         lsmask[j,i] = m.is_land(lon,lat,lsmask=True)
 m.drawlsmask(land_color='coral',ocean_color='aqua',lsmask=lsmask,lsmask_lons=lons,lsmask_lats=lats,lakes=True)
-plt.title('2.5 degree land-sea mask')
+plt.title('%s minute degree land-sea mask' % minutes)
+#lsmask.tofile('%sminlsmask_gshhs_%s.dat' % (minutes, resolution))
+
 plt.show()
