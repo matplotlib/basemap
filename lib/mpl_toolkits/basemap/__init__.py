35470b51523e9429d26cefc911dca843264581b9
mollyswanson <molly.swanson@gmail.com>
2011-11-29 16:55:11 -0500

    fixes bug in drawparallels when the radius of the sphere of the projection is set to something other than the default.

--- lib/mpl_toolkits/basemap/__init__.py
+++ lib/mpl_toolkits/basemap/__init__.py
@@ -835,7 +835,7 @@ class Basemap(object):
                 xd = (x[1:]-x[0:-1])**2
                 yd = (y[1:]-y[0:-1])**2
                 dist = np.sqrt(xd+yd)
-                split = dist > 5000000.
+                split = dist > 5000000./6370997.0*self.projparams['R']
                 if np.sum(split) and self.projection not in _cylproj:
                     ind = (np.compress(split,np.squeeze(split*np.indices(xd.shape)))+1).tolist()
                     iprev = 0
