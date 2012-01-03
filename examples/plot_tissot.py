eafe5c77a05d0f6d24bfb61c2be3a7d46c411b25
Jeff Whitaker <jswhit@fastmail.fm>
2012-01-03 12:01:39 -0700

    add new projections.

--- examples/plot_tissot.py
+++ examples/plot_tissot.py
@@ -18,35 +18,32 @@ if basemap_version < '0.99.1':
 # create Basemap instances with several different projections
 m1 = Basemap(llcrnrlon=-180,llcrnrlat=-80,urcrnrlon=180,urcrnrlat=80,
               projection='cyl')
-m2 = Basemap(lon_0=-60,lat_0=45,projection='ortho')
-m3 = Basemap(llcrnrlon=-180,llcrnrlat=-70,urcrnrlon=180,urcrnrlat=70,
+m2 = Basemap(llcrnrlon=-180,llcrnrlat=-80,urcrnrlon=180,urcrnrlat=80,
+              projection='mill')
+m3 = Basemap(lon_0=-60,lat_0=45,projection='ortho')
+m4 = Basemap(llcrnrlon=-180,llcrnrlat=-80,urcrnrlon=180,urcrnrlat=80,
              projection='merc',lat_ts=20)
-m4 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='npstere')
-m5 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='nplaea')
-m6 = Basemap(lon_0=0,projection='moll')
-m7 = Basemap(lon_0=0,projection='robin')
-m8 = Basemap(lon_0=0,projection='hammer')
-m9 = Basemap(lon_0=0,projection='mbtfpq')
-m10 = Basemap(lon_0=270,lat_0=90,boundinglat=10,projection='npaeqd')
+m5 = Basemap(lon_0=270,lat_0=90,boundinglat=5,projection='npstere')
+m6 = Basemap(lon_0=270,lat_0=90,boundinglat=5,projection='nplaea')
+m7 = Basemap(lon_0=0,projection='moll')
+m8 = Basemap(lon_0=0,projection='robin')
+m9 = Basemap(lon_0=0,projection='hammer')
+m10 = Basemap(lon_0=0,projection='mbtfpq')
+m11 = Basemap(lon_0=0,projection='eck4')
+m12 = Basemap(lon_0=0,projection='kav7')
+m13 = Basemap(lon_0=270,lat_0=90,boundinglat=5,projection='npaeqd')
 
-for m in [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10]:
+#for m in [m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13]:
+for m in [m6]:
     # make a new figure.
     fig = plt.figure()
     # draw "circles" at specified longitudes and latitudes.
-    for parallel in range(-60,61,30):
-        for meridian in range(-165,166,30):
+    for parallel in range(-70,71,20):
+        for meridian in range(-150,151,60):
             poly = m.tissot(meridian,parallel,6,100,facecolor='green',zorder=10,alpha=0.5)
     # draw meridians and parallels.
-    if m.projection != 'ortho':
-        labels = [1,0,0,0]
-    else:
-        labels = [0,0,0,0]
-    m.drawparallels(np.arange(-60,61,30),labels=labels)
-    if m.projection not in ['moll','hammer','mbtfpq','ortho']:
-        labels = [0,0,0,1]
-    else:
-        labels = [0,0,0,0]
-    m.drawmeridians(np.arange(-180,180,60),labels=labels)
+    m.drawparallels(np.arange(-60,61,30),labels=[1,0,0,0])
+    m.drawmeridians(np.arange(-180,180,60),labels=[0,0,0,1])
     # draw coastlines, fill continents, plot title.
     m.drawcoastlines()
     m.drawmapboundary(fill_color='aqua') 
