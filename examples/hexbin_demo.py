0b5d5a035897cbfd459b50b30b3a5c7962ea91c1
Jeff Whitaker <jswhit@fastmail.fm>
2012-02-01 14:38:34 -0700

    add histogram2d version

--- examples/hexbin_demo.py
+++ examples/hexbin_demo.py
@@ -3,10 +3,13 @@ from numpy.random import uniform
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.basemap import Basemap
+
 # create north polar stereographic basemap
 m = Basemap(lon_0=270, boundinglat=20, projection='npstere',round=True)
+
 # number of points to plot.
-npts = 20000
+npts = 10000
+bins = 40
 # generate random points on a sphere,
 # so that every small area on the sphere is expected
 # to have the same number of points.
@@ -24,12 +27,34 @@ x, y = m(lons, lats)
 xscaled = 4.*(x-0.5*(m.xmax-m.xmin))/m.xmax
 yscaled = 4.*(y-0.5*(m.ymax-m.ymin))/m.ymax
 z = xscaled*np.exp(-xscaled**2-yscaled**2)
-# make plot
-CS = plt.hexbin(x,y,C=z,gridsize=50,cmap=plt.cm.jet)
+
+# make plot using hexbin
+fig = plt.figure()
+CS = plt.hexbin(x,y,C=z,gridsize=bins,cmap=plt.cm.jet)
 # draw coastlines, lat/lon lines.
 m.drawcoastlines()
 m.drawparallels(np.arange(0,81,20))
 m.drawmeridians(np.arange(-180,181,60))
 m.colorbar() # draw colorbar
 plt.title('hexbin demo')
+
+# use histogram2d instead of hexbin.
+fig = plt.figure()
+bincount, xedges, yedges = np.histogram2d(x, y, bins=bins)
+mask = bincount == 0
+# reset zero values to one to avoid divide-by-zero
+bincount = np.where(bincount == 0, 1, bincount)
+H, xedges, yedges = np.histogram2d(x, y, bins=bins, weights=z)
+H = np.ma.masked_where(mask, H/bincount)
+# set color of masked values to white (hexbin does this by default)
+palette = plt.cm.jet
+palette.set_bad('white', 1.0)
+CS = m.pcolormesh(xedges,yedges,H.T,shading='flat',cmap=palette)
+# draw coastlines, lat/lon lines.
+m.drawcoastlines()
+m.drawparallels(np.arange(0,81,20))
+m.drawmeridians(np.arange(-180,181,60))
+m.colorbar() # draw colorbar
+plt.title('histogram2d demo')
+
 plt.show()
