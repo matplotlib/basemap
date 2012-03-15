c94e0305787682a6f55a032c1214ee188a4dc3d9
Jeff Whitaker <jswhit@fastmail.fm>
2012-03-15 14:55:14 -0600

    fix comments.

--- examples/make_inset.py
+++ examples/make_inset.py
@@ -2,10 +2,8 @@ from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
-import numpy as np
 
-
-# Set up inset map
+# Set up the primary map
 fig = plt.figure()
 ax = fig.add_subplot(111)
 bmap = Basemap(projection='lcc',width=6000.e3,height=4000.e3,lon_0=-90,lat_0=40,ax=ax)
@@ -16,13 +14,13 @@ bmap.drawmapboundary(fill_color='aqua')
 bmap.drawcoastlines()
 plt.title('map with an inset showing where the map is')
 
-# Inset South America Location Map
+# axes for inset map.
 axin = inset_axes(bmap.ax,width="30%",height="30%",loc=4)
 
+# inset map is global, with primary map projection region drawn on it.
 omap = Basemap(projection='ortho',lon_0=-105,lat_0=40,ax=axin,anchor='NE')
 omap.drawcountries(color='white')
 omap.fillcontinents(color='gray') #color = 'coral'               
-
 bx, by = omap(bmap.boundarylons, bmap.boundarylats)
 xy = list(zip(bx,by))
 mapboundary = Polygon(xy,edgecolor='red',linewidth=2,fill=False)
