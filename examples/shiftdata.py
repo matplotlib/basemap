fdc31d3b0fd48eb476ddb1712c1fe55d6bc92ce0
Jeff Whitaker <jswhit@fastmail.fm>
2012-07-10 10:10:57 -0600

    update

--- examples/shiftdata.py
+++ examples/shiftdata.py
@@ -2,15 +2,14 @@ from mpl_toolkits.basemap import Basemap,shiftgrid
 import numpy as np
 import matplotlib.pyplot as plt
 # read in topo data (on a regular lat/lon grid)
-# (skip last column of data)
-etopo=np.loadtxt('etopo20data.gz')[:,:-1]
-lons=np.loadtxt('etopo20lons.gz')[:-1]
+etopo=np.loadtxt('etopo20data.gz')
+lons=np.loadtxt('etopo20lons.gz')
 lats=np.loadtxt('etopo20lats.gz')
 lons, lats = np.meshgrid(lons, lats)
 # create Basemap instance.
 m = Basemap(projection='kav7',lon_0=0)
 # shift data and longitudes to fit map region
-etopo, lons = m.shiftdata(etopo, lons)
+lons, etopo = m.shiftdata(lons, etopo)
 # transform lats/lons to map projection coords
 x, y = m(lons, lats)
 # make filled contour plot
