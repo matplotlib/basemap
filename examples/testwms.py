7690c69bb2220f35c70911e304daab982be78013
Jeff Whitaker <jswhit@fastmail.fm>
2012-08-25 20:54:31 -0600

    example showing how to use WMS to fetch background image.

---
+++ examples/testwms.py
@@ -0,0 +1,43 @@
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import urllib2
+import numpy as np
+
+plt.figure()
+lon1 = -10; lon2 = 30; lat1 = 30; lat2 = 60
+basemap_url =\
+"http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_Imagery_World_2D/MapServer/export?\
+bbox=%d,%d,%d,%d&\
+bboxSR=4326&\
+imageSR=4326&\
+size=800,600&\
+dpi=128&\
+format=png32&\
+f=image" % (lon1, lat1, lon2, lat2)
+m = Basemap(projection='cyl',llcrnrlon=lon1,urcrnrlon=lon2,\
+            llcrnrlat=lat1,urcrnrlat=lat2,resolution='i')
+m.drawcoastlines()
+m.drawmeridians(np.arange(-5,26,5),labels=[0,0,0,1],color='y')
+m.drawparallels(np.arange(25,56,5),labels=[1,0,0,0],color='y')
+m.imshow(plt.imread(urllib2.urlopen(basemap_url)),origin='upper')
+
+width = 12000.e3
+plt.figure()
+basemap_url =\
+"http://server.arcgisonline.com/ArcGIS/rest/services/ESRI_Imagery_World_2D/MapServer/export?\
+bbox=%d,%d,%d,%d&\
+bboxSR=3412&\
+imageSR=3412&\
+size=800,800&\
+dpi=128&\
+format=png32&\
+f=image" % (-width/2,-width/2,width/2,width/2)
+m =\
+Basemap(projection='stere',resolution='i',lon_0=0,lat_0=-90,lat_ts=-70,\
+        width=width,height=width,rsphere=(6378273,6356889.449))
+m.imshow(plt.imread(urllib2.urlopen(basemap_url)),origin='upper')
+m.drawmeridians(np.arange(-180,180,30),labels=[0,0,0,1],color='y')
+m.drawparallels(np.arange(-80,-0,10),labels=[1,0,0,0],color='y')
+m.drawcoastlines()
+
+plt.show()
