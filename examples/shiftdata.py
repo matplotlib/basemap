2f9e25d5510ea8688cb7f122d4f74bd579bc6df3
Jeff Whitaker <jswhit@fastmail.fm>
2012-09-19 09:24:05 -0600

    update

--- examples/shiftdata.py
+++ examples/shiftdata.py
@@ -8,26 +8,18 @@ bm.latlon_default=True
 etopo=np.loadtxt('etopo20data.gz')
 lons=np.loadtxt('etopo20lons.gz')
 lats=np.loadtxt('etopo20lats.gz')
+# mask land regions.
 etopo = ma.masked_where(etopo > 0, etopo)
 lons, lats = np.meshgrid(lons, lats)
 # create Basemap instance.
 m = bm.Basemap(projection='kav7',lon_0=0)
-# can either shift data manually...
-# shift data and longitudes to fit map region
-#lons, etopo = m.shiftdata(lons, etopo)
-# transform lats/lons to map projection coords
-#x, y = m(lons, lats)
-# make filled contour plot
-#cs = m.contourf(x,y,etopo,30,cmap=plt.cm.jet)
-# or let contourf to it for you by passing lons/lats and
-# setting latlon=True.
+# latlon_default=True, so contourf expecting lons,lats (not x,y)
+# data automatically shifted in longitude to fit map projection region.
 cs = m.contourf(lons,lats,etopo,30,cmap=plt.cm.jet)
-#cs = m.pcolormesh(lons,lats,etopo,shading='flat')
-#cs = m.pcolor(lons,lats,etopo,shading='flat')
 # draw coastlines.
 m.drawcoastlines()
 # draw parallels and meridians.
 m.drawparallels(np.arange(-60.,90.,30.),labels=[1,0,0,0])
 m.drawmeridians(np.arange(0.,360.,60.),labels=[0,0,0,1],fontsize=12)
-plt.title('test shiftdata method')
+plt.title('test latlon=True')
 plt.show()
