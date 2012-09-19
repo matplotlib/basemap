69d2ea87fd697c5c05e0a8b5cd33b1772d6cad06
Jeff Whitaker <jswhit@fastmail.fm>
2012-09-19 16:00:37 -0600

    use pcolormesh instead of pcolor for speed.

--- doc/users/figures/plotsst.py
+++ doc/users/figures/plotsst.py
@@ -17,34 +17,24 @@ ice = dataset.variables['ice'][timeindex,:].squeeze()
 # read lats and lons (representing centers of grid boxes).
 lats = dataset.variables['lat'][:]
 lons = dataset.variables['lon'][:]
-# shift lats, lons so values represent edges of grid boxes
-# (as pcolor expects).
-delon = lons[1]-lons[0]; delat = lats[1]-lats[0]
-lons = (lons - 0.5*delon).tolist()
-lons.append(lons[-1]+delon)
-lons = np.array(lons,np.float64)
-lats = (lats - 0.5*delat).tolist()
-lats.append(lats[-1]+delat)
-lats = np.array(lats,np.float64)
+lons, lats = np.meshgrid(lons,lats)
 # create figure, axes instances.
 fig = plt.figure()
 ax = fig.add_axes([0.05,0.05,0.9,0.9])
-# create Basemap instance for Robinson projection.
+# create Basemap instance.
 # coastlines not used, so resolution set to None to skip
 # continent processing (this speeds things up a bit)
-m = Basemap(projection='robin',lon_0=lons.mean(),resolution=None)
-# compute map projection coordinates of grid.
-x, y = m(*np.meshgrid(lons, lats))
+m = Basemap(projection='kav7',lon_0=0,resolution=None)
 # draw line around map projection limb.
 # color background of map projection region.
 # missing values over land will show up this color.
 m.drawmapboundary(fill_color='0.3')
 # plot sst, then ice with pcolor
-im1 = m.pcolor(x,y,sst,shading='flat',cmap=plt.cm.jet)
-im2 = m.pcolor(x,y,ice,shading='flat',cmap=plt.cm.gist_gray)
+im1 = m.pcolormesh(lons,lats,sst,shading='flat',cmap=plt.cm.jet,latlon=True)
+im2 = m.pcolormesh(lons,lats,ice,shading='flat',cmap=plt.cm.gist_gray,latlon=True)
 # draw parallels and meridians, but don't bother labelling them.
-m.drawparallels(np.arange(-90.,120.,30.))
-m.drawmeridians(np.arange(0.,420.,60.))
+m.drawparallels(np.arange(-90.,99.,30.))
+m.drawmeridians(np.arange(-180.,180.,60.))
 # add colorbar
 cb = m.colorbar(im1,"bottom", size="5%", pad="2%")
 # add a title.
