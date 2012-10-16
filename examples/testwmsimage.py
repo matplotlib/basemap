677dcc1707213040e2c2ab281bc7160c14778137
Jeff Whitaker <jswhit@fastmail.fm>
2012-10-16 12:20:32 -0600

    add example from Rich Signell showing how to overlay an image from a WMS server on a map.

---
+++ examples/testwmsimage.py
@@ -0,0 +1,35 @@
+"""
+example showing how to use OWSlib to retrieve an image
+from a WMS server and display it on a map
+"""
+from mpl_toolkits.basemap import Basemap
+from owslib.wms import WebMapService
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import urllib2
+lon_min = -118.8; lon_max = -108.6
+lat_min = 22.15;  lat_max = 32.34
+m = Basemap(llcrnrlon=lon_min, urcrnrlat=lat_max, urcrnrlon=lon_max,
+            llcrnrlat=lat_min,resolution='i',epsg=4326)
+urlbase='http://motherlode.ucar.edu:8080/thredds/wms/fmrc/NCEP/NAM/CONUS_12km/NCEP-NAM-CONUS_12km-noaaport_best.ncd?'
+wms = WebMapService(urlbase)
+today = datetime.utcnow().strftime('%Y-%m-%dT00:00:00.000Z')
+img = wms.getmap(layers=['Temperature_height_above_ground'],
+                 styles=['boxfill/rainbow'],
+                 time=today,srs='EPSG:4326',
+                 bbox=(lon_min,lat_min,lon_max,lat_max),
+                 size=(500,500),format='image/png',elevation='2')
+# add on the additional custom WMS arguments supported by ncWMS to
+# define color range, colorbands and scale type 
+url = img.url +\
+      '&COLORSCALERANGE=271.2%2C308&NUMCOLORBANDS=20&LOGSCALE=false'
+cs=m.imshow(imread(urllib2.urlopen(url)),origin='upper')
+m.drawcoastlines(linewidth=0.25)
+parallels = np.arange(20,36,2.)
+a=m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10)
+# draw meridians
+meridians = np.arange(-120,-100,2.)
+b=m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10)
+plt.show()
