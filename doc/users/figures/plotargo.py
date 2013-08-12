5a26093e4b80f1cf9663f25195c2c2a46ad03619
Jeff Whitaker <jswhit@fastmail.fm>
2013-08-11 19:23:03 -0600

    retrieve data with urllib

--- doc/users/figures/plotargo.py
+++ doc/users/figures/plotargo.py
@@ -2,10 +2,11 @@ from netCDF4 import Dataset, num2date
 import time, calendar, datetime, numpy
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-# data downloaded from
-# http://coastwatch.pfeg.noaa.gov/erddap/tabledap/apdrcArgoAll.html via
-# http://coastwatch.pfeg.noaa.gov/erddap/tabledap/apdrcArgoAll.nc?longitude,latitude,time&longitude>=0&longitude<=360&latitude>=-90&latitude<=90&time>=2010-01-01&time<=2010-01-08&distinct()
-dset = Dataset('apdrcArgoAll_af28_ee2b_1d10.nc')
+import urllib, os
+# data downloaded from the form at
+# http://coastwatch.pfeg.noaa.gov/erddap/tabledap/apdrcArgoAll.html 
+filename, headers = urllib.urlretrieve('http://coastwatch.pfeg.noaa.gov/erddap/tabledap/apdrcArgoAll.nc?longitude,latitude,time&longitude>=0&longitude<=360&latitude>=-90&latitude<=90&time>=2010-01-01&time<=2010-01-08&distinct()')
+dset = Dataset(filename)
 lats = dset.variables['latitude'][:]
 lons = dset.variables['longitude'][:]
 time = dset.variables['time']
@@ -13,6 +14,8 @@ times = time[:]
 t1 = times.min(); t2 = times.max()
 date1 = num2date(t1, units=time.units)
 date2 = num2date(t2, units=time.units)
+dset.close()
+os.remove(filename)
 # draw map with markers for float locations
 m = Basemap(projection='hammer',lon_0=180)
 x, y = m(lons,lats)
