af73697a7315bf510d5bfc470af9b16a480c5f10
Sylvain Bellemare <sbellem@gmail.com>
2015-02-23 22:31:47 +0100

    made style changes (pep8)
    the key points touched were: indentation, blank lines, whitespace
    
    changes made based on https://www.python.org/dev/peps/pep-0008/

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import subprocess
 import sys
 
 major, minor1, minor2, s, tmp = sys.version_info
-if major==2 and minor1<4 or major<2:
+if major == 2 and minor1 < 4 or major < 2:
     raise SystemExit("""matplotlib and the basemap toolkit require Python 2.4 or later.""")
-from numpy.distutils.core  import setup, Extension
+from numpy.distutils.core import setup, Extension
 from distutils.util import convert_path
 from distutils import ccompiler, sysconfig
 try:
@@ -15,10 +15,11 @@ try:
 except ImportError:
     from distutils.command.build_py import build_py
 
+
 def checkversion(GEOS_dir):
     """check geos C-API header file (geos_c.h)"""
     try:
-        f = open(os.path.join(GEOS_dir,'include/geos_c.h'))
+        f = open(os.path.join(GEOS_dir, 'include/geos_c.h'))
     except IOError:
         return None
     geos_version = None
@@ -31,7 +32,7 @@ def checkversion(GEOS_dir):
 if 'GEOS_DIR' in os.environ:
     GEOS_dir = os.environ.get('GEOS_DIR')
 else:
-# set GEOS_dir manually here if automatic detection fails.
+    # set GEOS_dir manually here if automatic detection fails.
     GEOS_dir = None
 
 user_home = os.path.expanduser('~')
@@ -47,8 +48,8 @@ if GEOS_dir is None:
         if geos_version is None or geos_version < '"3.1.1"':
             continue
         else:
-            sys.stdout.write('GEOS lib (version %s) found in %s\n' %\
-                    (geos_version[1:-1],direc))
+            sys.stdout.write('GEOS lib (version %s) found in %s\n' %
+                             (geos_version[1:-1], direc))
             GEOS_dir = direc
             break
 else:
@@ -67,31 +68,31 @@ set GEOS_DIR to /usr/local), or edit the setup.py script
 manually and set the variable GEOS_dir (right after the line
 that says "set GEOS_dir manually here".""" % "', '".join(geos_search_locations))
 else:
-    geos_include_dirs=[os.path.join(GEOS_dir,'include'),numpy.get_include()]
-    geos_library_dirs=[os.path.join(GEOS_dir,'lib'),os.path.join(GEOS_dir,'lib64')]
+    geos_include_dirs = [os.path.join(GEOS_dir, 'include'), numpy.get_include()]
+    geos_library_dirs = [os.path.join(GEOS_dir, 'lib'), os.path.join(GEOS_dir, 'lib64')]
 
 # proj4 and geos extensions.
 deps = glob.glob('src/*.c')
-deps.remove(os.path.join('src','_proj.c'))
-deps.remove(os.path.join('src','_geoslib.c'))
+deps.remove(os.path.join('src', '_proj.c'))
+deps.remove(os.path.join('src', '_geoslib.c'))
 
-packages          = ['mpl_toolkits','mpl_toolkits.basemap']
+packages = ['mpl_toolkits', 'mpl_toolkits.basemap']
 namespace_packages = ['mpl_toolkits']
-package_dirs       = {'':'lib'}
-extensions = [Extension("mpl_toolkits.basemap._proj",deps+['src/_proj.c'],include_dirs = ['src'],)]
+package_dirs = {'': 'lib'}
+extensions = [Extension("mpl_toolkits.basemap._proj", deps+['src/_proj.c'], include_dirs=['src'],)]
 # can't install _geoslib in mpl_toolkits.basemap namespace,
 # or Basemap objects won't be pickleable.
 if sys.platform == 'win32':
-# don't use runtime_library_dirs on windows (workaround
-# for a distutils bug - http://bugs.python.org/issue2437).
-    #extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
-    extensions.append(Extension("_geoslib",['src/_geoslib.c'],
+    # don't use runtime_library_dirs on windows (workaround
+    # for a distutils bug - http://bugs.python.org/issue2437).
+    # extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
+    extensions.append(Extension("_geoslib", ['src/_geoslib.c'],
                                 library_dirs=geos_library_dirs,
                                 include_dirs=geos_include_dirs,
                                 libraries=['geos']))
 else:
-    #extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
-    extensions.append(Extension("_geoslib",['src/_geoslib.c'],
+    # extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
+    extensions.append(Extension("_geoslib", ['src/_geoslib.c'],
                                 library_dirs=geos_library_dirs,
                                 runtime_library_dirs=geos_library_dirs,
                                 include_dirs=geos_include_dirs,
@@ -100,8 +101,8 @@ else:
 # Specify all the required mpl data
 # create pyproj binary datum shift grid files.
 pathout =\
-os.path.join('lib',os.path.join('mpl_toolkits',os.path.join('basemap','data')))
-if sys.argv[1] not in ['sdist','clean']:
+    os.path.join('lib', os.path.join('mpl_toolkits', os.path.join('basemap', 'data')))
+if sys.argv[1] not in ['sdist', 'clean']:
     cc = ccompiler.new_compiler()
     sysconfig.get_config_vars()
     sysconfig.customize_compiler(cc)
@@ -110,46 +111,46 @@ if sys.argv[1] not in ['sdist','clean']:
     execname = 'nad2bin'
     cc.link_executable(objects, execname)
     llafiles = glob.glob('datumgrid/*.lla')
-    cmd = os.path.join(os.getcwd(),execname)
+    cmd = os.path.join(os.getcwd(), execname)
     for f in llafiles:
         fout = os.path.basename(f.split('.lla')[0])
-        fout = os.path.join(pathout,fout)
+        fout = os.path.join(pathout, fout)
         strg = '%s %s < %s' % (cmd, fout, f)
         sys.stdout.write('executing %s\n' % strg)
-        subprocess.call(strg,shell=True)
-datafiles = glob.glob(os.path.join(pathout,'*'))
-datafiles = [os.path.join('data',os.path.basename(f)) for f in datafiles]
-package_data = {'mpl_toolkits.basemap':datafiles}
+        subprocess.call(strg, shell=True)
+datafiles = glob.glob(os.path. join(pathout, '*'))
+datafiles = [os.path.join('data', os.path.basename(f)) for f in datafiles]
+package_data = {'mpl_toolkits.basemap': datafiles}
 
 __version__ = "1.0.8"
 setup(
-  name              = "basemap",
-  version           = __version__,
-  description       = "Plot data on map projections with matplotlib",
-  long_description  = """
+    name="basemap",
+    version=__version__,
+    description="Plot data on map projections with matplotlib",
+    long_description="""
   An add-on toolkit for matplotlib that lets you plot data
   on map projections with coastlines, lakes, rivers and political boundaries.
   See http://www.scipy.org/wikis/topical_software/Maps for an
   example of what it can do.""",
-  url               = "http://matplotlib.sourceforge.net/toolkits.html",
-  download_url      = "https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-{0}/basemap-{0}.tar.gz".format(__version__),
-  author            = "Jeff Whitaker",
-  author_email      = "jeffrey.s.whitaker@noaa.gov",
-  platforms         = ["any"],
-  license           = "OSI Approved",
-  keywords          = ["python","plotting","plots","graphs","charts","GIS","mapping","map projections","maps"],
-  classifiers       = ["Development Status :: 5 - Production/Stable",
-                       "Intended Audience :: Science/Research",
-                       "License :: OSI Approved",
-                       "Programming Language :: Python",
-                       "Programming Language :: Python :: 3",
-                       "Topic :: Scientific/Engineering :: Visualization",
-                       "Topic :: Software Development :: Libraries :: Python Modules",
-                       "Operating System :: OS Independent"],
-  packages          = packages,
-  namespace_packages = namespace_packages,
-  package_dir       = package_dirs,
-  ext_modules       = extensions,
-  cmdclass = {'build_py': build_py},
-  package_data = package_data
-  )
+    url="http://matplotlib.sourceforge.net/toolkits.html",
+    download_url="https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-{0}/basemap-{0}.tar.gz".format(__version__),
+    author="Jeff Whitaker",
+    author_email="jeffrey.s.whitaker@noaa.gov",
+    platforms=["any"],
+    license="OSI Approved",
+    keywords=["python", "plotting", "plots", "graphs", "charts", "GIS", "mapping", "map projections", "maps"],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 3",
+                 "Topic :: Scientific/Engineering :: Visualization",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Operating System :: OS Independent"],
+    packages=packages,
+    namespace_packages=namespace_packages,
+    package_dir=package_dirs,
+    ext_modules=extensions,
+    cmdclass={'build_py': build_py},
+    package_data=package_data
+)
