e072dd7d7c3ee3637c25682c70fbf5e76e1e6686
Oleksandr Huziy <guziy.sasha@gmail.com>
2015-10-25 04:24:00 -0400

    fix issue 209: failure to reproject C-non-contiguous numpy arrays

--- lib/mpl_toolkits/basemap/pyproj.py
+++ lib/mpl_toolkits/basemap/pyproj.py
@@ -517,11 +517,13 @@ def _copytobuffer(x):
             return _copytobuffer_return_scalar(x)
         else:
             try:
+                import numpy as np
                 # typecast numpy arrays to double.
                 # (this makes a copy - which is crucial
                 #  since buffer is modified in place)
                 x.dtype.char
-                inx = x.astype('d')
+                inx = x.astype('d', order="C")
+
                 # inx,isfloat,islist,istuple
                 return inx,False,False,False
             except:
