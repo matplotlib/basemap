fef60e7172cbbcec4d6ee5d25a3500522ffa75ae
Micah Cochran <micahcochran@users.noreply.github.com>
2015-11-30 16:33:18 -0600

    Update setup.py: removing PROJ.4
    Removed sections for building PROJ.4.
    
    Using a different (clearer?) way to check sys.version_info for python version 2.4 or greater.

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import sys, glob, os, subprocess
 
-major, minor1, minor2, s, tmp = sys.version_info
-if major==2 and minor1<4 or major<2:
+if sys.version_info < (2, 4):
     raise SystemExit("""matplotlib and the basemap toolkit require Python 2.4 or later.""")
 
 from distutils.dist import Distribution
@@ -81,56 +80,25 @@ else:
     geos_include_dirs=[os.path.join(GEOS_dir,'include'),inc_dirs]
     geos_library_dirs=[os.path.join(GEOS_dir,'lib'),os.path.join(GEOS_dir,'lib64')]
 
-# proj4 and geos extensions.
-deps = glob.glob('src/*.c')
-deps.remove(os.path.join('src','_proj.c'))
-deps.remove(os.path.join('src','_geoslib.c'))
-
 packages          = ['mpl_toolkits','mpl_toolkits.basemap']
 namespace_packages = ['mpl_toolkits']
 package_dirs       = {'':'lib'}
-extensions = [Extension("mpl_toolkits.basemap._proj",deps+['src/_proj.c'],include_dirs = ['src'],)]
-# can't install _geoslib in mpl_toolkits.basemap namespace,
-# or Basemap objects won't be pickleable.
-if sys.platform == 'win32':
-# don't use runtime_library_dirs on windows (workaround
-# for a distutils bug - http://bugs.python.org/issue2437).
-    #extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
-    extensions.append(Extension("_geoslib",['src/_geoslib.c'],
-                                library_dirs=geos_library_dirs,
-                                include_dirs=geos_include_dirs,
-                                libraries=['geos']))
-else:
-    #extensions.append(Extension("mpl_toolkits.basemap._geoslib",['src/_geoslib.c'],
-    extensions.append(Extension("_geoslib",['src/_geoslib.c'],
-                                library_dirs=geos_library_dirs,
-                                runtime_library_dirs=geos_library_dirs,
-                                include_dirs=geos_include_dirs,
-                                libraries=['geos_c']))
 
 # Specify all the required mpl data
 # create pyproj binary datum shift grid files.
 pathout =\
 os.path.join('lib',os.path.join('mpl_toolkits',os.path.join('basemap','data')))
-if sys.argv[1] not in ['sdist','clean']:
-    cc = ccompiler.new_compiler()
-    sysconfig.get_config_vars()
-    sysconfig.customize_compiler(cc)
-    cc.set_include_dirs(['src'])
-    objects = cc.compile(['nad2bin.c', 'src/pj_malloc.c'])
-    execname = 'nad2bin'
-    cc.link_executable(objects, execname)
-    llafiles = glob.glob('datumgrid/*.lla')
-    cmd = os.path.join(os.getcwd(),execname)
-    for f in llafiles:
-        fout = os.path.basename(f.split('.lla')[0])
-        fout = os.path.join(pathout,fout)
-        strg = '%s %s < %s' % (cmd, fout, f)
-        sys.stdout.write('executing %s\n' % strg)
-        subprocess.call(strg,shell=True)
+
 datafiles = glob.glob(os.path.join(pathout,'*'))
 datafiles = [os.path.join('data',os.path.basename(f)) for f in datafiles]
 package_data = {'mpl_toolkits.basemap':datafiles}
+
+requirements = [
+  "numpy>=1.2.1", 
+  "matplotlib>=1.0.0",
+  "pyproj >= 1.9.3", 
+  "pyshp >= 1.2.0"
+]
 
 __version__ = "1.0.8"
 setup(
@@ -146,7 +114,7 @@ setup(
   download_url      = "https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-{0}/basemap-{0}.tar.gz".format(__version__),
   author            = "Jeff Whitaker",
   author_email      = "jeffrey.s.whitaker@noaa.gov",
-  install_requires  = ["numpy>=1.2.1", "matplotlib>=1.0.0"],
+  install_requires  = requirements,
   platforms         = ["any"],
   license           = "OSI Approved",
   keywords          = ["python","plotting","plots","graphs","charts","GIS","mapping","map projections","maps"],
@@ -161,7 +129,6 @@ setup(
   packages          = packages,
   namespace_packages = namespace_packages,
   package_dir       = package_dirs,
-  ext_modules       = extensions,
   cmdclass = {'build_py': build_py},
   package_data = package_data
   )
