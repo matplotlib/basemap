97e064075e49224b23257e3e0761d8fde75282cc
Joe Kington <joferkington@gmail.com>
2016-07-16 09:50:38 -0500

    Update style of git commands to GMT 5.x (i.e. "gmt pscoast" instead of "pscoast")

--- utils/dumpbounds.sh
+++ utils/dumpbounds.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 area=10000000000000000000000000000000
-pscoast -M -Dc -R0/360/-90/90 -N1 -JQ180/4.5i -W -A${area} > countries_c.txt
-pscoast -M -Dc -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_c.txt
-pscoast -M -Dl -R0/360/-90/90 -N1 -JQ180/4.5i -W -A${area} > countries_l.txt
-pscoast -M -Dl -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_l.txt
-pscoast -M -Di -R0/360/-90/90 -N1 -JQ180/4.5i -W -A${area} > countries_i.txt
-pscoast -M -Di -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_i.txt
-pscoast -M -Dh -R0/360/-90/90 -N1 -JQ180/4.5i -W -A${area} > countries_h.txt
-pscoast -M -Dh -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_h.txt
-pscoast -M -Df -R0/360/-90/90 -N1 -JQ180/4.5i -W -A${area} > countries_f.txt
-pscoast -M -Df -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_f.txt
-pscoast -M -Dh -R0/360/-90/90 -N2 -JQ180/4.5i -W -A${area} > states_h.txt
-pscoast -M -Dc -R0/360/-90/90 -Ir -JQ180/4.5i -W -A${area} > rivers_c.txt
-pscoast -M -Dl -R0/360/-90/90 -Ir -JQ180/4.5i -W -A${area} > rivers_l.txt
-pscoast -M -Di -R0/360/-90/90 -Ir -JQ180/4.5i -W -A${area} > rivers_i.txt
-pscoast -M -Dh -R0/360/-90/90 -Ir -JQ180/4.5i -W -A${area} > rivers_h.txt
-pscoast -M -Df -R0/360/-90/90 -Ir -JQ180/4.5i -W -A${area} > rivers_f.txt
+gmt pscoast -M -Dc -R0/360/-90/90 -N1 -A${area} > countries_c.txt
+gmt pscoast -M -Dc -R0/360/-90/90 -N2 -A${area} > states_c.txt
+gmt pscoast -M -Dl -R0/360/-90/90 -N1 -A${area} > countries_l.txt
+gmt pscoast -M -Dl -R0/360/-90/90 -N2 -A${area} > states_l.txt
+gmt pscoast -M -Di -R0/360/-90/90 -N1 -A${area} > countries_i.txt
+gmt pscoast -M -Di -R0/360/-90/90 -N2 -A${area} > states_i.txt
+gmt pscoast -M -Dh -R0/360/-90/90 -N1 -A${area} > countries_h.txt
+gmt pscoast -M -Dh -R0/360/-90/90 -N2 -A${area} > states_h.txt
+gmt pscoast -M -Df -R0/360/-90/90 -N1 -A${area} > countries_f.txt
+gmt pscoast -M -Df -R0/360/-90/90 -N2 -A${area} > states_f.txt
+gmt pscoast -M -Dh -R0/360/-90/90 -N2 -A${area} > states_h.txt
+gmt pscoast -M -Dc -R0/360/-90/90 -Ir -A${area} > rivers_c.txt
+gmt pscoast -M -Dl -R0/360/-90/90 -Ir -A${area} > rivers_l.txt
+gmt pscoast -M -Di -R0/360/-90/90 -Ir -A${area} > rivers_i.txt
+gmt pscoast -M -Dh -R0/360/-90/90 -Ir -A${area} > rivers_h.txt
+gmt pscoast -M -Df -R0/360/-90/90 -Ir -A${area} > rivers_f.txt
