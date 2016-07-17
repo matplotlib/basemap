4a52e4917c99e269258443cb5d0bd94252009bd8
Joe Kington <joferkington@gmail.com>
2016-07-16 19:22:12 -0500

    Add gshhg extraction directly into dumpbounds script

--- utils/dumpbounds.sh
+++ utils/dumpbounds.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+set -e
 
+# This should be set to wherever you extract the GSHHG data to.
+gshhs_dir=./gshhg/
 area=10000000000000000000000000000000
+
+gmt gshhg -A${area} $gshhs_dir/gshhs_c.b > gshhs_c.txt
+gmt gshhg -A${area} $gshhs_dir/gshhs_l.b > gshhs_l.txt
+gmt gshhg -A${area} $gshhs_dir/gshhs_i.b > gshhs_i.txt
+gmt gshhg -A${area} $gshhs_dir/gshhs_h.b > gshhs_h.txt
+gmt gshhg -A${area} $gshhs_dir/gshhs_f.b > gshhs_f.txt
+
 gmt pscoast -M -Rg -Dc -N1 -A${area} > countries_c.txt
 gmt pscoast -M -Rg -Dc -N2 -A${area} > states_c.txt
 gmt pscoast -M -Rg -Dl -N1 -A${area} > countries_l.txt
