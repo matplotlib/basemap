ef3ec4689b35a9fe1c0a2da53ded8b5fdcdb8163
Víctor Molina García <molinav@users.noreply.github.com>
2021-12-02 19:31:52 +0100

    Set alpha version 1.3.0a1 for data packages

--- packages/basemap_data/setup.py
+++ packages/basemap_data/setup.py
@@ -75,7 +75,7 @@ setup(**{
     "name":
         "basemap_data",
     "version":
-        "1.2.2+dev",
+        "1.3.0a1",
     "license":
         "GNU General Public License v2 or later (GPLv2+)",
     "description":
