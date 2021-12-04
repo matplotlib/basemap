b8f10dc7aec63c88454ee2df48daa29f2a67d660
Víctor Molina García <molinav@users.noreply.github.com>
2021-12-04 11:12:24 +0100

    Move _geoslib source code one folder up

--- packages/basemap/setup.py
+++ packages/basemap/setup.py
@@ -118,7 +118,7 @@ ext_modules = [
         "name":
             "_geoslib",
         "sources": [
-            "src/_geoslib/_geoslib.pyx",
+            "src/_geoslib.pyx",
         ],
         "libraries": [
             "geos_c",
