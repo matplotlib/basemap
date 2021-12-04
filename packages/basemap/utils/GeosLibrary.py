2a7ba42f237268b87ef901035e9ab5b7d0ff89ec
Víctor Molina García <molinav@users.noreply.github.com>
2021-12-04 11:21:53 +0100

    Add license header to GeosLibrary file

--- packages/basemap/utils/GeosLibrary.py
+++ packages/basemap/utils/GeosLibrary.py
@@ -1,4 +1,21 @@
 #! /usr/bin/env python
+# -*- coding: utf8 -*-
+#
+# Copyright (c) 2021 Víctor Molina García
+
+# GeosLibrary.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# GeosLibrary.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with GeosLibrary.py. If not, see <https://www.gnu.org/licenses/>.
+#
 """Helper class to download and build the GEOS library."""
 
 import io
