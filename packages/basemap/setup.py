8e8db8e2e17a5f85e24532505359d2789fff362d
Kyle Sunden <git@ksunden.space>
2023-06-08 16:42:59 -0500

    Convert to new style namespace packages

--- packages/basemap/setup.py
+++ packages/basemap/setup.py
@@ -9,7 +9,7 @@ import sys
 import glob
 import warnings
 from setuptools import setup
-from setuptools import find_packages
+from setuptools import find_namespace_packages
 from setuptools.command.sdist import sdist
 from setuptools.dist import Distribution
 from setuptools.extension import Extension
@@ -213,7 +213,7 @@ setup(**{
     "package_dir":
         {"": "src"},
     "packages":
-        find_packages(where="src"),
+        find_namespace_packages(where="src"),
     "ext_modules":
         ext_modules,
     "data_files":
--- packages/basemap/src/mpl_toolkits/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import (absolute_import, division, print_function)
-
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    pass # must not have setuptools
