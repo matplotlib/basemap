bb0d522355e9332c2b3cda34f09c6200a4b83cbe
Víctor Molina García <molinav@users.noreply.github.com>
2023-11-19 23:25:21 +0100

    Add tests for Basemap.bluemarble and Basemap.etopo

--- packages/basemap/test/mpl_toolkits/basemap/test_Basemap.py
+++ packages/basemap/test/mpl_toolkits/basemap/test_Basemap.py
@@ -23,16 +23,24 @@ mpl_version = tuple(map(int, mpl.__version__.split(".")[:2]))
 class TestMplToolkitsBasemapBasemap(unittest.TestCase):
     """Unittest class for the :mod:`mpl_toolkits.basemap.Basemap` class."""
 
-    @unittest.skipIf(PIL is None, reason="pillow unavailable")
-    def test_shadedrelief(self, axs=None, axslen0=10):
-        """Test drawing a map with a shaded relief image as background."""
+    def setUp(self):
+        """Define the setup of test scope variables."""
+
+    def tearDown(self):
+        """Define the teardown of test scope variables."""
+
+        axs_obj = plt.gca()
+        axs_obj.clear()
+
+    def _test_basemap_data_warpimage(self, method, axs=None, axslen0=10):
+        """Test drawing a map background from :mod:`basemap_data`."""
 
         axs_obj = plt.gca() if axs is None else axs
         axs_children = axs_obj.get_children()
         self.assertEqual(len(axs_children), axslen0)
 
         bmap = Basemap(ax=axs, projection="moll", resolution=None, lon_0=0)
-        img = bmap.shadedrelief(ax=axs, scale=0.1)
+        img = getattr(bmap, method)(ax=axs, scale=0.1)
         self.assertIsInstance(img, AxesImage)
 
         flag = int(mpl_version < (3, 5))
@@ -42,6 +50,41 @@ class TestMplToolkitsBasemapBasemap(unittest.TestCase):
         self.assertIsInstance(axs_children[2 - flag], Polygon)
         self.assertIsInstance(axs_children[(axslen0 + 1) * flag], AxesImage)
         self.assertIs(axs_children[(axslen0 + 1) * flag], img)
+
+    @unittest.skipIf(PIL is None, reason="pillow unavailable")
+    def test_bluemarble(self, axs=None, axslen0=10):
+        """Test drawing a map with a blue marble image as background."""
+
+        method = "bluemarble"
+        self._test_basemap_data_warpimage(method, axs=axs, axslen0=axslen0)
+
+    @unittest.skipIf(PIL is None, reason="pillow unavailable")
+    def test_bluemarble_with_custom_axes(self):
+        """Test drawing a map with a blue marble image as background."""
+
+        _, axs = plt.subplots()
+        self.test_bluemarble(axs=axs, axslen0=10)
+
+    @unittest.skipIf(PIL is None, reason="pillow unavailable")
+    def test_etopo(self, axs=None, axslen0=10):
+        """Test drawing a map with an ETOPO relief image as background."""
+
+        method = "etopo"
+        self._test_basemap_data_warpimage(method, axs=axs, axslen0=axslen0)
+
+    @unittest.skipIf(PIL is None, reason="pillow unavailable")
+    def test_etopo_with_custom_axes(self):
+        """Test drawing a map with an ETOPO relief image as background."""
+
+        _, axs = plt.subplots()
+        self.test_etopo(axs=axs, axslen0=10)
+
+    @unittest.skipIf(PIL is None, reason="pillow unavailable")
+    def test_shadedrelief(self, axs=None, axslen0=10):
+        """Test drawing a map with a shaded relief image as background."""
+
+        method = "shadedrelief"
+        self._test_basemap_data_warpimage(method, axs=axs, axslen0=axslen0)
 
     @unittest.skipIf(PIL is None, reason="pillow unavailable")
     def test_shadedrelief_with_custom_axes(self):
