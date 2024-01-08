cba0ebcd77063d004e8f6ff4734a1c62047ae8ef
Víctor Molina García <molinav@users.noreply.github.com>
2024-01-08 13:05:32 +0100

    Update copyright notices
    With this commit, I would like to state more clearly that contributions
    are currently performed on behalf of the Matplotlib development team.
    The selection of start/end years for the copyright headers are based
    on the first/last time that each copyright holder committed to this
    repository.

--- packages/basemap/utils/GeosLibrary.py
+++ packages/basemap/utils/GeosLibrary.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (c) 2021-2024 Víctor Molina García
-
+#
 # GeosLibrary.py is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-
+#
 # GeosLibrary.py is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
-
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with GeosLibrary.py. If not, see <https://www.gnu.org/licenses/>.
 #
