31440f075a20a8e762cdd83af97acec622428ad4
Kenji7SenSei <cho.seongyeon17@gmail.com>
2025-05-07 02:02:29 -0400

    Issue #610 changed the central longitude of projection to 0

---
+++ examples/shade.py
@@ -0,0 +1,25 @@
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+# lon_0 is central longitude of projection.
+# resolution = 'c' means use crude resolution coastlines.
+m = Basemap(projection='robin',lon_0=180,resolution='c')
+
+m.drawcoastlines()
+m.fillcontinents(color='coral',lake_color='aqua')
+
+# draw parallels and meridians.
+m.drawparallels(np.arange(-90.,120.,30.))
+m.drawmeridians(np.arange(0.,360.,60.))
+m.drawmapboundary(fill_color='aqua')
+
+date = datetime(2024,10,10,6)
+
+m.nightshade(date,alpha=0.2)
+
+plt.title("Robinson Projection")
+plt.savefig('test.png')
+
