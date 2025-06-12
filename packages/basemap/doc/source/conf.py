d7d7a0c69709b6ba051fb6cbb481e20fa186e278
Víctor Molina García <molinav@users.noreply.github.com>
2025-06-12 23:42:37 +0200

    Fix deprecated use of source_suffix in Sphinx conf.py file

--- packages/basemap/doc/source/conf.py
+++ packages/basemap/doc/source/conf.py
@@ -73,7 +73,11 @@ templates_path = ["_templates"]
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = [".rst", ".md"]
-source_suffix = ".rst"
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "restructuredtext",
+    ".md": "markdown",
+}
 
 # The master toctree document.
 master_doc = "index"
