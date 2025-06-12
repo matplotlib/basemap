3acfbc78408bc9a5a2aa05c83b7d1d52245c2ea4
Víctor Molina García <molinav@users.noreply.github.com>
2025-06-12 21:47:28 +0200

    Fix docstring indentation after content fix

--- packages/basemap/src/mpl_toolkits/basemap/__init__.py
+++ packages/basemap/src/mpl_toolkits/basemap/__init__.py
@@ -4836,10 +4836,11 @@ class Basemap(object):
                          [lon_0-180, lon_0+180] range.
         ================ ======================================================
 
-        If datain is given, returns ``lonsout, dataout`` (longitudes and data shifted to fit in the interval
-[lon_0-180, lon_0+180]); otherwise, returns just the shifted longitudes. If
-transformed longitudes lie outside the map projection region, data is
-masked and longitudes are set to 1.e30.
+        If datain is given, returns ``lonsout, dataout`` (longitudes and data
+        shifted to fit in the interval [lon_0-180, lon_0+180]); otherwise,
+        returns just the shifted longitudes. If transformed longitudes lie
+        outside the map projection region, data is masked and longitudes are
+        set to 1.e30.
         """
         if lon_0 is None and 'lon_0' not in self.projparams:
             raise ValueError('lon_0 keyword must be provided')
