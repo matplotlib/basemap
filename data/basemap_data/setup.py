fbd080ff51eefdf973221e1794b7dba1dc44565b
Víctor Molina García <molinav@users.noreply.github.com>
2025-06-13 15:23:43 +0200

    Make the maintainers metadata more general

--- data/basemap_data/setup.py
+++ data/basemap_data/setup.py
@@ -85,9 +85,9 @@ setup(**{
     "author_email":
         "jeffrey.s.whitaker@noaa.gov",
     "maintainer":
-        "Víctor Molina García",
+        "The Matplotlib development team",
     "maintainer_email":
-        "molinav@users.noreply.github.com",
+        "matplotlib-users@python.org",
     "license":
         "GNU Lesser General Public License v3 or later (LGPLv3+)",
     "license_files": [
